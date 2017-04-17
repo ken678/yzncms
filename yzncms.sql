f142f188b9e9c3208f2d4d3a68f8d3ea389c7358
ken678 <530765310@qq.com>
2017-04-17 09:58:01 +0800

    附上数据库 Signed-off-by: ken678 <530765310@qq.com>

---
+++ yzncms.sql
@@ -0,0 +1,112 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50547
+Source Host           : localhost:3306
+Source Database       : yzncms
+
+Target Server Type    : MYSQL
+Target Server Version : 50547
+File Encoding         : 65001
+
+Date: 2017-04-17 09:56:52
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `yzn_admin`
+-- ----------------------------
+DROP TABLE IF EXISTS `yzn_admin`;
+CREATE TABLE `yzn_admin` (
+  `userid` smallint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `username` varchar(20) DEFAULT NULL COMMENT '管理账号',
+  `password` varchar(40) DEFAULT NULL COMMENT '管理密码',
+  `encrypt` varchar(6) DEFAULT NULL COMMENT '加密因子',
+  `nickname` char(16) NOT NULL COMMENT '昵称',
+  `last_login_time` int(10) unsigned DEFAULT '0' COMMENT '最后登录时间',
+  `last_login_ip` bigint(20) unsigned DEFAULT '0' COMMENT '最后登录IP',
+  PRIMARY KEY (`userid`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=gbk;
+
+-- ----------------------------
+-- Records of yzn_admin
+-- ----------------------------
+INSERT INTO `yzn_admin` VALUES ('1', 'admin', '29b2d14df82d7db68dc31faa9af3e7fee7499546', 'djvlfg', '御宅男', '1492153576', '2130706433');
+
+-- ----------------------------
+-- Table structure for `yzn_config`
+-- ----------------------------
+DROP TABLE IF EXISTS `yzn_config`;
+CREATE TABLE `yzn_config` (
+  `id` smallint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT '配置ID',
+  `name` varchar(30) NOT NULL DEFAULT '' COMMENT '配置名称',
+  `title` varchar(50) NOT NULL DEFAULT '' COMMENT '配置说明',
+  `type` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '配置分组',
+  `value` text NOT NULL COMMENT '配置值',
+  `sort` smallint(3) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of yzn_config
+-- ----------------------------
+INSERT INTO `yzn_config` VALUES ('1', 'site_title', '网站标题', '1', 'Yzncms内容管理框架 - Powered by Yzncms', '1');
+INSERT INTO `yzn_config` VALUES ('4', 'site_keyword', '网站关键字', '1', 'ThinkPHP,tp5.0,yzncms,内容管理系统', '2');
+INSERT INTO `yzn_config` VALUES ('5', 'site_description', '网站描述', '1', 'Yzncms内容管理框架,一套简单，易用，面向开发者的内容管理框,采用TP5.0框架开发', '3');
+INSERT INTO `yzn_config` VALUES ('6', 'site_name', '网站名称', '1', 'Yzncms内容管理框架', '0');
+INSERT INTO `yzn_config` VALUES ('7', 'icp', 'icp', '2', '苏ICP备15017030', '0');
+
+-- ----------------------------
+-- Table structure for `yzn_config_field`
+-- ----------------------------
+DROP TABLE IF EXISTS `yzn_config_field`;
+CREATE TABLE `yzn_config_field` (
+  `fid` smallint(6) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `fieldname` varchar(30) NOT NULL DEFAULT '' COMMENT '字段名',
+  `type` varchar(10) NOT NULL DEFAULT '' COMMENT '配置表单类型',
+  `setting` mediumtext COMMENT '其他设置',
+  `createtime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  PRIMARY KEY (`fid`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of yzn_config_field
+-- ----------------------------
+INSERT INTO `yzn_config_field` VALUES ('2', 'icp', 'input', 'a:4:{s:5:\"title\";s:3:\"icp\";s:4:\"tips\";s:9:\"备案号\";s:5:\"style\";s:0:\"\";s:6:\"option\";s:24:\"选项名称1|选项值1\";}', '1492154575');
+
+-- ----------------------------
+-- Table structure for `yzn_menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `yzn_menu`;
+CREATE TABLE `yzn_menu` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '菜单ID',
+  `title` varchar(50) NOT NULL DEFAULT '' COMMENT '标题',
+  `pid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '上级分类ID',
+  `app` char(20) NOT NULL DEFAULT '' COMMENT '应用标识',
+  `controller` char(20) NOT NULL DEFAULT '' COMMENT '控制器标识',
+  `action` char(20) NOT NULL DEFAULT '' COMMENT '方法标识',
+  `parameter` char(255) NOT NULL DEFAULT '' COMMENT '附加参数',
+  `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态',
+  `tip` varchar(255) NOT NULL DEFAULT '' COMMENT '提示',
+  `group` varchar(50) DEFAULT NULL COMMENT '分组',
+  `is_dev` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否开发者可见',
+  `listorder` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT '排序ID',
+  PRIMARY KEY (`id`),
+  KEY `pid` (`pid`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of yzn_menu
+-- ----------------------------
+INSERT INTO `yzn_menu` VALUES ('1', '设置', '0', 'Admin', 'Setting', 'index', '', '1', '', '', '0', '1');
+INSERT INTO `yzn_menu` VALUES ('2', '内容', '0', 'Admin', 'Content', 'index', '', '1', '', '', '0', '1');
+INSERT INTO `yzn_menu` VALUES ('3', '用户', '0', 'Admin', 'Member', 'index', '', '1', '', '', '0', '1');
+INSERT INTO `yzn_menu` VALUES ('9', '扩展配置', '5', 'Admin', 'Config', 'extend', '', '1', '', '', '0', '5');
+INSERT INTO `yzn_menu` VALUES ('5', '站点配置', '10', 'Admin', 'Config', 'index', '', '1', '', '', '0', '1');
+INSERT INTO `yzn_menu` VALUES ('6', '管理员管理', '1', 'Admin', 'Admin_manage', 'index', '', '1', '', '', '0', '1');
+INSERT INTO `yzn_menu` VALUES ('7', '角色管理', '1', 'Admin', 'Role', 'index', '', '1', '', '', '0', '1');
+INSERT INTO `yzn_menu` VALUES ('10', '设置', '1', 'Admin', 'Setting', 'index', '', '1', '', null, '0', '0');
