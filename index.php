fd93cb0320e12c8a8169d73d0bfa5fc8af286e7a
ken678 <530765310@qq.com>
2017-08-15 17:34:05 +0800

    取消自定义配置路径

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 if(version_compare(PHP_VERSION,'5.4.0','<'))  die('require PHP > 5.4.0 !');
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/apps/');
-define('CONF_PATH', __DIR__.'/apps/common/conf/');//重新定义配置路径
 
 // 加载框架引导文件
 require __DIR__ . '/thinkphp/start.php';
