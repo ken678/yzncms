2891cea4115939ed3732e254fa872cf7b0078d6a
ken <530765310@qq.com>
2018-01-01 19:49:07 +0800

    ...

--- public/index.php
+++ public/index.php
@@ -12,8 +12,17 @@
 // [ 应用入口文件 ]
 namespace think;
 
+//PHP版本检查
+header("Content-type: text/html; charset=utf-8");
+if (version_compare(PHP_VERSION, '5.6.0', '<')) {
+    die('require PHP > 5.6.0 !');
+}
+
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
+
+//定义模板目录
+define('TEMPLATE_PATH', __DIR__ . '/../templates/');
 
 // 支持事先使用静态方法设置Request对象和Config对象
 
