a3ca19edd4d2f5a7ac81881489470ecfe7d10856
ken678 <530765310@qq.com>
2018-05-10 15:44:50 +0800

    更新

--- public/static/admin/js/common.js
+++ public/static/admin/js/common.js
@@ -12,6 +12,44 @@ layui.use(['element', 'layer', 'form'], function() {
         layer.closeAll('tips')
     })
 
+    //通用添加
+    $(".com_add_btn").click(function(){
+       addNews('',$(this).attr('url'));
+    })
+
+    //添加文章
+    function addNews(edit,url){
+        var index = layui.layer.open({
+            title : "新增数据",
+            type : 2,
+            content : url,
+            success : function(layero, index){
+                var body = layui.layer.getChildFrame('body', index);
+                if(edit){
+                    body.find(".newsName").val(edit.newsName);
+                    body.find(".abstract").val(edit.abstract);
+                    body.find(".thumbImg").attr("src",edit.newsImg);
+                    body.find("#news_content").val(edit.content);
+                    body.find(".newsStatus select").val(edit.newsStatus);
+                    body.find(".openness input[name='openness'][title='"+edit.newsLook+"']").prop("checked","checked");
+                    body.find(".newsTop input[name='newsTop']").prop("checked",edit.newsTop);
+                    form.render();
+                }
+                setTimeout(function(){
+                    layui.layer.tips('点击此处返回文章列表', '.layui-layer-setwin .layui-layer-close', {
+                        tips: 3
+                    });
+                },500)
+            }
+        })
+        layui.layer.full(index);
+        //改变窗口大小时，重置弹窗的宽高，防止超出可视区域（如F12调出debug的操作）
+        $(window).on("resize",function(){
+            layui.layer.full(index);
+        })
+    }
+
+    //通用表单post提交
     $('.ajax-post').on('click', function(e) {
         var target, query, form1;
         var target_form = $(this).attr('target-form');
