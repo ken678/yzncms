5beca48a85989cc40d6658e71bd0b1e10c8988a2
ken678 <530765310@qq.com>
2018-09-20 09:19:03 +0800

    更新

--- application/admin/controller/Adminlog.php
+++ application/admin/controller/Adminlog.php
@@ -14,23 +14,39 @@
 // +----------------------------------------------------------------------
 namespace app\admin\controller;
 
+use app\admin\model\Adminlog as Adminlog_Model;
 use app\common\controller\Adminbase;
 use think\Db;
 
 class Adminlog extends Adminbase
 {
+    protected function initialize()
+    {
+        parent::initialize();
+        $this->Adminlog_Model = new Adminlog_Model;
+    }
     //日志首页
     public function index()
     {
         if ($this->request->isAjax()) {
             $limit = $this->request->param('limit/d', 10);
             $page = $this->request->param('page/d', 10);
-            $data = model("adminlog")->page($page, $limit)->order('id', 'desc')->select();
-            $total = model("adminlog")->order('id', 'desc')->count();
+            $data = $this->Adminlog_Model->page($page, $limit)->order('id', 'desc')->select();
+            $total = $this->Adminlog_Model->order('id', 'desc')->count();
             $result = array("code" => 0, "count" => $total, "data" => $data);
             return json($result);
         }
         return $this->fetch();
-
     }
+
+    //删除一个月前的操作日志
+    public function deletelog()
+    {
+        if ($this->Adminlog_Model->deleteAMonthago()) {
+            $this->success("删除日志成功！");
+        } else {
+            $this->error("删除日志失败！");
+        }
+    }
+
 }
