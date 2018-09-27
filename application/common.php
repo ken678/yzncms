087b4bca180b45e9b0e9e54d54529fde8e5e7235
ken678 <530765310@qq.com>
2018-09-27 16:18:07 +0800

    更新

--- application/common.php
+++ application/common.php
@@ -137,39 +137,6 @@ function time_format($time = null, $format = 'Y-m-d H:i')
 }
 
 /**
- * select返回的数组进行整数映射转换
- *
- * @param array $map  映射关系二维数组  array(
- *                                          '字段名1'=>array(映射关系数组),
- *                                          '字段名2'=>array(映射关系数组),
- *                                           ......
- *                                       )
- * @author 朱亚杰 <zhuyajie@topthink.net>
- * @return array
- *
- *  array(
- *      array('id'=>1,'title'=>'标题','status'=>'1','status_text'=>'正常')
- *      ....
- *  )
- *
- */
-function int_to_string(&$data, $map = array('status' => array(1 => '正常', -1 => '删除', 0 => '禁用', 2 => '未审核', 3 => '草稿')))
-{
-    if ($data === false || $data === null) {
-        return $data;
-    }
-    $data = (array) $data;
-    foreach ($data as $key => $row) {
-        foreach ($map as $col => $pair) {
-            if (isset($row[$col]) && isset($pair[$row[$col]])) {
-                $data[$key][$col . '_text'] = $pair[$row[$col]];
-            }
-        }
-    }
-    return $data;
-}
-
-/**
  * 把返回的数据集转换成Tree
  * @param array $list 要转换的数据集
  * @param string $pid parent标记字段
