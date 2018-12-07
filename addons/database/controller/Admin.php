b51b7ca1c6e199523fe22671c1f5a4f2ccb98696
ken678 <530765310@qq.com>
2018-12-07 17:18:50 +0800

    更新

--- addons/database/controller/Admin.php
+++ addons/database/controller/Admin.php
@@ -26,7 +26,21 @@ class Admin extends Adminaddon
     //数据库备份
     public function index()
     {
+        if ($this->request->isAjax()) {
+            $list = db()->query('SHOW TABLE STATUS');
+            $list = array_map('array_change_key_case', $list); //全部小写
+            $result = array("code" => 0, "data" => $list);
+            return json($result);
+        }
         return $this->fetch();
+    }
+
+    //备份数据库
+    public function export()
+    {
+        var_dump(111);
+        exit();
+
     }
 
 }
