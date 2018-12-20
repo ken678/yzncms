1095be54a8271f48647d435f898696535baaaad7
ken678 <530765310@qq.com>
2018-12-20 15:02:26 +0800

    更新

---
+++ application/cms/install/cms.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS `yzn_category`;
+CREATE TABLE `yzn_category` (
+  `catid` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '栏目ID',
+  `module` varchar(15) NOT NULL DEFAULT '' COMMENT '所属模块',
+  `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '类别',
+  `modelid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '模型ID',
+  `domain` varchar(200) NOT NULL DEFAULT '' COMMENT '栏目绑定域名',
+  `parentid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '父ID',
+  `arrparentid` varchar(255) NOT NULL DEFAULT '' COMMENT '所有父ID',
+  `child` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否存在子栏目，1存在',
+  `arrchildid` mediumtext COMMENT '所有子栏目ID',
+  `catname` varchar(30) NOT NULL DEFAULT '' COMMENT '栏目名称',
+  `image` varchar(100) NOT NULL DEFAULT '' COMMENT '栏目图片',
+  `description` mediumtext COMMENT '栏目描述',
+  `parentdir` varchar(100) NOT NULL DEFAULT '' COMMENT '父目录',
+  `catdir` varchar(30) NOT NULL DEFAULT '' COMMENT '栏目目录',
+  `url` varchar(100) NOT NULL DEFAULT '' COMMENT '链接地址',
+  `hits` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '栏目点击数',
+  `setting` mediumtext COMMENT '相关配置信息',
+  `listorder` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  `ismenu` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否显示',
+  `sethtml` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否生成静态',
+  `letter` varchar(30) NOT NULL DEFAULT '' COMMENT '栏目拼音',
+  PRIMARY KEY (`catid`),
+  KEY `module` (`module`,`parentid`,`listorder`,`catid`),
+  KEY `siteid` (`type`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='栏目表';
