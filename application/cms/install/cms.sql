d84420057c74ff4cbff46dbf900a5585abd02e63
ken678 <530765310@qq.com>
2018-12-21 09:34:09 +0800

    更新

--- application/cms/install/cms.sql
+++ application/cms/install/cms.sql
@@ -25,3 +25,53 @@ CREATE TABLE `yzn_category` (
   KEY `module` (`module`,`parentid`,`listorder`,`catid`),
   KEY `siteid` (`type`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='栏目表';
+
+DROP TABLE IF EXISTS `yzn_model`;
+CREATE TABLE `yzn_model` (
+  `modelid` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(30) NOT NULL DEFAULT '' COMMENT '模型名称',
+  `description` char(100) NOT NULL DEFAULT '' COMMENT '描述',
+  `tablename` char(20) NOT NULL DEFAULT '' COMMENT '表名',
+  `setting` text COMMENT '配置信息',
+  `addtime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
+  `items` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '信息数',
+  `enablesearch` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否开启全站搜索',
+  `disabled` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否禁用 1禁用',
+  `default_style` char(30) NOT NULL DEFAULT '' COMMENT '风格',
+  `category_template` char(30) NOT NULL DEFAULT '' COMMENT '栏目模板',
+  `list_template` char(30) NOT NULL DEFAULT '' COMMENT '列表模板',
+  `show_template` char(30) NOT NULL DEFAULT '' COMMENT '内容模板',
+  `js_template` varchar(30) NOT NULL DEFAULT '' COMMENT 'JS模板',
+  `sort` tinyint(3) NOT NULL DEFAULT '0' COMMENT '排序',
+  `type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '模块标识',
+  PRIMARY KEY (`modelid`),
+  KEY `type` (`type`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='内容模型列表';
+
+DROP TABLE IF EXISTS `yzn_model_field`;
+CREATE TABLE `yzn_model_field` (
+  `fieldid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `modelid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '模型ID',
+  `field` varchar(20) NOT NULL DEFAULT '' COMMENT '字段名',
+  `name` varchar(30) NOT NULL DEFAULT '' COMMENT '别名',
+  `tips` text COMMENT '字段提示',
+  `css` varchar(30) NOT NULL DEFAULT '' COMMENT '表单样式',
+  `pattern` varchar(255) NOT NULL DEFAULT '' COMMENT '数据校验正则',
+  `errortips` varchar(255) NOT NULL DEFAULT '' COMMENT '数据校验未通过的提示信息',
+  `formtype` varchar(20) NOT NULL DEFAULT '' COMMENT '字段类型',
+  `define` varchar(128) NOT NULL COMMENT '字段定义',
+  `setting` mediumtext COMMENT '额外设置',
+  `iscore` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否内部字段 1是',
+  `issystem` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否系统字段 1 是',
+  `isunique` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '值唯一',
+  `isbase` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '作为基本信息',
+  `issearch` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '作为搜索条件',
+  `isadd` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '在前台投稿中显示',
+  `isfulltext` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '作为全站搜索信息',
+  `isposition` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否入库到推荐位',
+  `listorder` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  `disabled` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '1 禁用 0启用',
+  PRIMARY KEY (`fieldid`),
+  KEY `modelid` (`modelid`,`disabled`),
+  KEY `field` (`field`,`modelid`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='模型字段列表';
