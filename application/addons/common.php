7b1988f075d4722d9288e9f7f94b403ff03ad487
ken678 <530765310@qq.com>
2018-12-28 10:23:43 +0800

    更新

--- application/addons/common.php
+++ application/addons/common.php
@@ -12,3 +12,18 @@
 // +----------------------------------------------------------------------
 // | 插件函数文件
 // +----------------------------------------------------------------------
+/**
+ * 检查插件控制器是否存在某操作
+ * @param string $name 插件名
+ * @param string $controller 控制器
+ * @param string $action 动作
+ * @author 蔡伟明 <314013107@qq.com>
+ * @return bool
+ */
+function plugin_action_exists($name = '', $controller = '', $action = '')
+{
+    if (strpos($name, '/')) {
+        list($name, $controller, $action) = explode('/', $name);
+    }
+    return method_exists("addons\\{$name}\\controller\\{$controller}", $action);
+}
--- application/addons/controller/Error.php
+++ application/addons/controller/Error.php
@@ -23,6 +23,9 @@ class Error
         $controller = \think\Loader::parseName($request->controller());
         $action = $request->action();
         $class = $request->param('isadmin/d') ? 'Admin' : 'Index';
+        if (!plugin_action_exists($controller, $class, $action)) {
+            throw new \think\Exception("插件{$controller}实例化错误！");
+        }
         $object = \think\Container::get("\\addons\\" . $controller . "\\controller\\{$class}");
         return $object->$action();
     }
