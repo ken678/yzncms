83db303ad35b5a3a4c330ec03d09f0f3c7bf4065
ken <530765310@qq.com>
2019-02-03 13:03:58 +0800

    更新

--- application/cms/taglib/Yzn.php
+++ application/cms/taglib/Yzn.php
@@ -31,58 +31,88 @@ class Yzn extends Taglib
         'category' => ['attr' => 'catid,cache,num,return,where,order', 'close' => 1, 'level' => 3],
     ];
 
-    protected function replaceVar($str)
-    {
-        if (preg_match_all("/(?:\[)(.*)(?:\])/iU", $str, $matches)) {
-            foreach ($matches[1] as $key => $vo) {
-                $realVal = $this->autoBuildVar($vo);
-                //不在开头
-                if (0 != strpos($str, $matches[0][$key])) {
-                    $realVal = '".' . $realVal;
-                }
-                //不在结尾
-                if (strpos($str, $matches[0][$key]) < strlen($str) - strlen($matches[0][$key])) {
-                    $realVal = $realVal . '."';
-                }
-                $str = str_replace($matches[0][$key], $realVal, $str);
-            }
-        }
-        return $str;
-    }
-
     public function tagCategory($tag, $content)
     {
         //每页显示总数
         $num = isset($tag['num']) ? (int) $tag['num'] : 10;
         //数据返回变量
         $return = isset($tag['return']) ? "data" : $tag['return'];
-        $where = isset($tag['where']) ? $tag['where'] : "status='1'";
+        $where = isset($tag['where']) ? $tag['where'] : "status=1";
         $order = isset($tag['order']) ? $tag['order'] : 'listorder,id desc';
         //缓存时间
         $cache = (int) $tag['cache'];
-        $where = $this->replaceVar($where);
-
-        $cacheID = to_guid_string($tag);
-        if ($cache && $array = Cache::get($cacheID)) {
-            return $array;
-        }
-
         if (isset($tag['catid'])) {
             $catid = (int) $tag['catid'];
-            //$where['parentid'] = $tag['catid'];
+            $where .= empty($where) ? "parentid = " . $catid : " AND parentid = " . $catid;
         }
-
         //拼接php代码
         $parseStr = '<?php ';
-        $parseStr .= '$db = model("Category");';
+        $parseStr .= '$cache = ' . $cache . ';';
+        $parseStr .= '$cacheID = to_guid_string(' . self::arr_to_html($tag) . ');';
+        $parseStr .= 'if($cache && $_return = Cache::get($cacheID)):';
+        $parseStr .= '$' . $return . ' = $_return;';
+        $parseStr .= 'else: ';
+        $parseStr .= '$db = db("Category");';
         //如果条件不为空，进行查库
         if (!empty($where)) {
             $parseStr .= '$' . $return . '= $db->where("' . $where . '")->limit(' . $num . ')->select();';
+            $parseStr .= 'if($cache):';
+            $parseStr .= 'Cache::set($cacheID, $' . $return . ', $cache);';
+            $parseStr .= 'endif;';
         }
-        $parseStr .= 'foreach($' . $return . ' as $key=>$vo): ?>';
+        $parseStr .= 'endif; ?>';
         $parseStr .= $content;
-        $parseStr .= '<?php endforeach; ?>';
         return $parseStr;
+    }
+
+    /**
+     * 转换数据为HTML代码
+     * @param array $data 数组
+     */
+    private static function arr_to_html($data)
+    {
+        if (is_array($data)) {
+            $str = 'array(';
+            foreach ($data as $key => $val) {
+                if (is_array($val)) {
+                    $str .= "'$key'=>" . self::arr_to_html($val) . ",";
+                } else {
+                    //如果是变量的情况
+                    if (strpos($val, '$') === 0) {
+                        $str .= "'$key'=>$val,";
+                    } else if (preg_match("/^([a-zA-Z_].*)\(/i", $val, $matches)) {
+//判断是否使用函数
+                        if (function_exists($matches[1])) {
+                            $str .= "'$key'=>$val,";
+                        } else {
+                            $str .= "'$key'=>'" . self::newAddslashes($val) . "',";
+                        }
+                    } else {
+                        $str .= "'$key'=>'" . self::newAddslashes($val) . "',";
+                    }
+                }
+            }
+            return $str . ')';
+        }
+        return false;
+    }
+
+    /**
+     * 返回经addslashes处理过的字符串或数组
+     * @param $string 需要处理的字符串或数组
+     * @return mixed
+     */
+    protected static function newAddslashes($string)
+    {
+        if (!is_array($string)) {
+            return addslashes($string);
+        }
+
+        foreach ($string as $key => $val) {
+            $string[$key] = $this->newAddslashes($val);
+        }
+
+        return $string;
     }
 
 }
