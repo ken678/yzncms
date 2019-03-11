b6bf401d0256bb822b5e844692485901b2ded31b
ken678 <530765310@qq.com>
2019-03-11 12:00:16 +0800

    更新

--- application/cms/controller/Tags.php
+++ application/cms/controller/Tags.php
@@ -31,8 +31,13 @@ class Tags extends Adminbase
     public function index()
     {
         if ($this->request->isAjax()) {
-            $data = $this->Tags->select();
-            $result = array("code" => 0, "data" => $data);
+            $_list = $this->Tags->withAttr('lastusetime', function ($value, $data) {
+                return date('Y-m-d H:i:s', $value);
+            })->withAttr('lasthittime', function ($value, $data) {
+                return date('Y-m-d H:i:s', $value);
+            })->order(['listorder', 'id' => 'desc'])->select();
+            $total = count($_list);
+            $result = array("code" => 0, "count" => $total, "data" => $_list);
             return json($result);
         }
         return $this->fetch();
@@ -52,7 +57,34 @@ class Tags extends Adminbase
      */
     public function delete()
     {
+        $tagid = $this->request->param('ids/a', null);
+        if (!is_array($tagid)) {
+            $tagid = array($tagid);
+        }
+        foreach ($tagid as $tid) {
+            $info = $this->Tags->where(array('id' => $tid))->find();
+            if (!empty($info)) {
+                if ($this->Tags->where(array('tag' => $info['tag']))->delete() !== false) {
+                    model('TagsContent')->where(array('tag' => $info['tag']))->delete();
+                }
+            }
+        }
+        $this->success("删除成功！");
+    }
 
+    /**
+     * tags排序
+     */
+    public function listorder()
+    {
+        $id = $this->request->param('id/d', 0);
+        $listorder = $this->request->param('value/d', 0);
+        $rs = $this->Tags->allowField(['listorder'])->isUpdate(true)->save(['id' => $id, 'listorder' => $listorder]);
+        if ($rs) {
+            $this->success("菜单排序成功！");
+        } else {
+            $this->error("菜单排序失败！");
+        }
     }
 
 }
