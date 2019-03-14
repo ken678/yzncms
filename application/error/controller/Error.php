dd0dda33cca8c99d4fc211241e1b99e095a9f554
ken678 <530765310@qq.com>
2019-03-14 12:35:25 +0800

    .

--- application/error/controller/Error.php
+++ application/error/controller/Error.php
@@ -14,12 +14,11 @@
 // +----------------------------------------------------------------------
 namespace app\error\controller;
 
+//空模块
 class Error
 {
-
     public function _empty()
     {
         abort(404, '应用模块不存在~');
     }
-
 }
