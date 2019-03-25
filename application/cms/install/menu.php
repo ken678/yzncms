2cd0064c45afac7bb48b8ef25e4f80722b0f224f
ken678 <530765310@qq.com>
2019-03-25 15:17:49 +0800

    ..

--- application/cms/install/menu.php
+++ application/cms/install/menu.php
@@ -4,7 +4,7 @@ return [
         //父菜单ID，NULL或者不写系统默认，0为顶级菜单
         "parentid" => 0,
         //地址，[模块/]控制器/方法
-        "route" => "cms/index/index",
+        "route" => "cms/cms/index1",
         //类型，1：权限认证+菜单，0：只作为菜单
         "type" => 0,
         //状态，1是显示，0不显示（需要参数的，建议不显示，例如编辑,删除等操作）
@@ -20,7 +20,7 @@ return [
         //子菜单列表
         "child" => [
             [
-                "route" => "cms/cms/index1",
+                "route" => "cms/cms/index2",
                 "type" => 1,
                 "status" => 1,
                 "name" => "内容管理",
