6626e2387e0d6fee02923a1fdce3a210f2afcd9a
ken678 <530765310@qq.com>
2019-03-26 11:52:57 +0800

    ..

--- application/member/controller/Member.php
+++ application/member/controller/Member.php
@@ -15,15 +15,49 @@
 namespace app\member\controller;
 
 use app\common\controller\Adminbase;
+use app\member\model\Member as Member_Model;
 use think\Db;
 
 class Member extends Adminbase
 {
+    //初始化
+    protected function initialize()
+    {
+        parent::initialize();
+        $this->Member_Model = new Member_Model;
+        //$this->groupCache = cache("Member_group"); //会员模型
+        //$this->groupsModel = cache("Model_Member"); //会员组
+    }
 
-    //
+    /**
+     * 会员列表
+     */
     public function manage()
     {
+        if ($this->request->isAjax()) {
+            $limit = $this->request->param('limit/d', 10);
+            $page = $this->request->param('page/d', 10);
+            $_list = $this->Member_Model->page($page, $limit)->select();
+            $total = count($_list);
+            $result = array("code" => 0, "count" => $total, "data" => $_list);
+            return json($result);
+
+        }
         return $this->fetch();
+    }
+
+    /**
+     * 会员增加
+     */
+    public function add()
+    {
+        if ($this->request->isPost()) {
+            $data = $this->request->post();
+
+        } else {
+            return $this->fetch();
+        }
+
     }
 
     //
