a4a98140cbad6ddba5efcee604210b1bce8c40f9
ken678 <530765310@qq.com>
2019-04-15 18:28:54 +0800

    ..

--- config/cookie.php
+++ config/cookie.php
@@ -14,17 +14,17 @@
 // +----------------------------------------------------------------------
 return [
     // cookie 名称前缀
-    'prefix'    => '',
+    'prefix' => 'think_',
     // cookie 保存时间
-    'expire'    => 0,
+    'expire' => 0,
     // cookie 保存路径
-    'path'      => '/',
+    'path' => '/',
     // cookie 有效域名
-    'domain'    => '',
+    'domain' => '',
     //  cookie 启用安全传输
-    'secure'    => false,
+    'secure' => false,
     // httponly设置
-    'httponly'  => '',
+    'httponly' => '',
     // 是否使用 setcookie
     'setcookie' => true,
 ];
