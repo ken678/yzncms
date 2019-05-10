943e5ac9e97a48a4125404c5cab1c63d8a654e94
马春伟 <530765310@qq.com>
2019-05-10 08:10:09 +0800

    ..

--- application/member/validate/Member.php
+++ application/member/validate/Member.php
@@ -23,7 +23,7 @@ class Member extends Validate
         'username|用户名' => 'unique:member|require|alphaDash|length:3,20',
         'nickname|昵称' => 'unique:member|chsDash|length:3,20',
         'password|密码' => 'require|length:3,20|confirm',
-        'email|邮箱' => 'require|email',
+        'email|邮箱' => 'unique:member|require|email',
         'groupid|会员组' => 'require|number',
     ];
 
