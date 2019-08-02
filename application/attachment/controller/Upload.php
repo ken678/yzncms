6cffe84331332dce21408707d506f92f428906f8
马春伟 <530765310@qq.com>
2019-08-02 15:54:55 +0800

    ..

--- application/attachment/controller/Upload.php
+++ application/attachment/controller/Upload.php
@@ -311,7 +311,7 @@ class Upload extends Base
 
         // 附件上传钩子，用于第三方文件上传扩展
         if (config('upload_driver')['key'] != 'local') {
-            $hook_result = Hook::listen('uploadAttachment', ['file' => $file, 'from' => $from, 'module' => $module], true);
+            $hook_result = Hook::listen('uploadAfter', ['file' => $file, 'from' => $from, 'module' => $module], true);
             if (false !== $hook_result) {
                 return $hook_result;
             }
