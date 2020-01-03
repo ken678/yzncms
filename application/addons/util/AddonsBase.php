c26611c7df8c7b564b8d2b77db7ed8d9a6a117e3
马春伟 <530765310@qq.com>
2020-01-03 11:48:05 +0800

    ..

--- application/addons/util/AddonsBase.php
+++ application/addons/util/AddonsBase.php
@@ -67,4 +67,30 @@ class AddonsBase extends Base
     {
         return $this->view->fetch($this->parseAddonTemplateFile($template, $this->addonPath), $vars, $config);
     }
+
+    /**
+     * 自动定位模板文件
+     * @access private
+     * @param  string $template 模板文件规则
+     * @param type $addonPath 插件目录
+     * @return string
+     */
+    private function parseAddonTemplateFile($template, $addonPath)
+    {
+        if (0 !== strpos($template, '/')) {
+            $template = str_replace(['/', ':'], DIRECTORY_SEPARATOR, $template);
+            if ($this->addonName) {
+                if ('' == $template) {
+                    // 如果模板文件名为空 按照默认规则定位
+                    $template = str_replace('.', DIRECTORY_SEPARATOR, $this->addonName) . DIRECTORY_SEPARATOR . $this->request->action();
+                } elseif (false === strpos($template, DIRECTORY_SEPARATOR)) {
+                    $template = str_replace('.', DIRECTORY_SEPARATOR, $this->addonName) . DIRECTORY_SEPARATOR . $template;
+                }
+            }
+        } else {
+            $template = str_replace(['/', ':'], DIRECTORY_SEPARATOR, substr($template, 1));
+        }
+        return $addonPath . 'view' . DIRECTORY_SEPARATOR . ltrim($template, '/') . '.html';
+
+    }
 }
