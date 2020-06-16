32a1101b34304b085363bff2b768ab7288de5c19
马春伟 <530765310@qq.com>
2020-06-16 18:07:33 +0800

    更新

---
+++ addons/easysms/Easysms.php
@@ -0,0 +1,146 @@
+<?php
+// +----------------------------------------------------------------------
+// | Yzncms [ 御宅男工作室 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://yzncms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 御宅男 <530765310@qq.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | 短信插件
+// +----------------------------------------------------------------------
+namespace addons\easysms;
+
+use app\addons\util\Addon;
+
+class Easysms extends Addon
+{
+    //插件信息
+    public $info = [
+        'name' => 'easysms',
+        'title' => '多通道短信发送插件',
+        'description' => '支持目前市面20+家短信服务商,自动轮询选择可用的服务商',
+        'status' => 1,
+        'author' => '御宅男',
+        'version' => '1.0.0',
+    ];
+
+    public $config = [
+        // HTTP 请求的超时时间（秒）
+        'timeout' => 5.0,
+
+        // 默认发送配置
+        'default' => [
+            // 网关调用策略，默认：顺序调用
+            'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+            // 默认可用的发送网关
+            'gateways' => [
+                //阿里云
+                'aliyun',
+            ],
+        ],
+        // 可用的网关配置
+        'gateways' => [
+            'errorlog' => [
+                'file' => '/tmp/easy-sms.log',
+            ],
+            //阿里云
+            'aliyun' => [
+                'access_key_id' => '',
+                'access_key_secret' => '',
+                'sign_name' => '',
+            ],
+        ],
+    ];
+
+    /**
+     * 插件安装方法
+     * @return bool
+     */
+    public function install()
+    {
+        return true;
+    }
+
+    /**
+     * 插件卸载方法
+     * @return bool
+     */
+    public function uninstall()
+    {
+        return true;
+    }
+
+    /**
+     * 短信发送行为
+     * @param   Sms     $params
+     * @return  boolean
+     */
+    public function smsSend($params)
+    {
+        $config = $this->getAddonConfig();
+
+        $easySms = new \Overtrue\EasySms\EasySms($this->config);
+        try {
+            $result = $easySms->send($params->mobile, [
+                'content' => '您的验证码为: ' . $params->code,
+                'template' => $config['template'][$params->event],
+                'data' => [
+                    'code' => $params->code,
+                ],
+            ]);
+        } catch (\Exception $exception) {
+            return false;
+        }
+        $is_suc = false;
+        foreach ($result as $v) {
+            if ($v['status'] == 'success') {
+                $is_suc = true;
+                break;
+            }
+        }
+        return $is_suc;
+    }
+
+    /**
+     * 短信发送通知
+     * @param   array   $params
+     * @return  boolean
+     */
+    public function smsNotice($params)
+    {
+        $easySms = new \Overtrue\EasySms\EasySms($this->config);
+        try {
+            $result = $easySms->send($params['mobile'], [
+                'content' => $params['msg'],
+                'template' => $params['template'],
+                'data' => isset($params['data']) ? $params['data'] : [],
+            ]);
+        } catch (\Exception $exception) {
+            return false;
+        }
+        $is_suc = false;
+        foreach ($result as $v) {
+            if ($v['status'] == 'success') {
+                $is_suc = true;
+                break;
+            }
+        }
+        return $is_suc;
+    }
+
+    /**
+     * 检测验证是否正确
+     * @param   Sms     $params
+     * @return  boolean
+     */
+    public function smsCheck($params)
+    {
+        return true;
+    }
+
+}
---
+++ addons/easysms/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    'template' => array(
+        'title' => '短信模板',
+        'type' => 'array',
+        'value' => array(
+            'register' => '',
+            'resetpwd' => '',
+            'changepwd' => '',
+            'profile' => '',
+            'actmobile' => '',
+        ),
+        'tip' => '',
+    ),
+);
