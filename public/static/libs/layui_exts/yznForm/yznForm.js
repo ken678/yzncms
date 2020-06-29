4556872f884776eb38d1a42ff4018ea962d2b66f
马春伟 <530765310@qq.com>
2020-06-29 12:40:49 +0800

    更新

--- public/static/libs/layui_exts/yznForm/yznForm.js
+++ public/static/libs/layui_exts/yznForm/yznForm.js
@@ -490,7 +490,7 @@ layui.define(['form', 'yzn', 'table', 'notice','element'], function(exports) {
     });*/
 
     //ajax get请求
-    /*$(document).on('click', '.ajax-get', function() {
+    $(document).on('click', '.ajax-get', function() {
         var that = $(this),
             href = !that.attr('data-href') ? that.attr('href') : that.attr('data-href'),
             refresh = !that.attr('refresh') ? 'true' : that.attr('refresh');
@@ -526,10 +526,10 @@ layui.define(['form', 'yzn', 'table', 'notice','element'], function(exports) {
             });
         };
         return false;
-    });*/
+    });
 
     //通用表单post提交
-    /*$('.ajax-post').on('click', function(e) {
+    $('.ajax-post').on('click', function(e) {
         var target, query, _form,
             target_form = $(this).attr('target-form'),
             that = this,
@@ -599,7 +599,7 @@ layui.define(['form', 'yzn', 'table', 'notice','element'], function(exports) {
             }
         });
         return false;
-    });*/
+    });
 
     exports(MOD_NAME, {});
 });
