639079bb1434ca240ae8eaa7909f11f6c6247cf7
马春伟 <530765310@qq.com>
2020-07-02 11:37:04 +0800

    更新

--- application/cms/controller/Category.php
+++ application/cms/controller/Category.php
@@ -59,7 +59,7 @@ class Category extends Adminbase
                 } else {
                     $v['modelname'] = '/';
                 }
-                $v['catname'] = '<a href="' . url('edit', ['id' => $v['id']]) . '"">' . $v['catname'] . '</a>';
+                $v['catname'] = '<a data-full="true" data-open="' . url('edit', ['id' => $v['id']]) . '"">' . $v['catname'] . '</a>';
                 if ($v['type'] == 1) {
                     $v['add_url'] = url("Category/singlepage", array("parentid" => $v['id']));
                 } elseif ($v['type'] == 2) {
