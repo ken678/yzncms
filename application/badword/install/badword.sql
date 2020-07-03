f1faaaf5bbfe171a33002cd828e82cd246798345
马春伟 <530765310@qq.com>
2020-07-03 17:27:12 +0800

    去除敏感词模块 已内置

--- application/badword/install/badword.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `yzn_badword`;
-CREATE TABLE `yzn_badword` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `badword` char(20) NOT NULL COMMENT '目标词',
-  `replaceword` char(20) NOT NULL DEFAULT '0' COMMENT '替换词',
-  `create_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `listorder` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
