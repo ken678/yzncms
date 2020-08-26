3a3a66502562f738500632b1b27b16115bd5acbb
yzncms <530765310@qq.com>
2020-08-26 11:33:09 +0800

    更新

--- application/common/controller/Adminbase.php
+++ application/common/controller/Adminbase.php
@@ -223,4 +223,113 @@ class Adminbase extends Base
         return [$page, $limit, $where, $excludes];
     }
 
+    /**
+     * Selectpage的实现方法
+     *
+     * 当前方法只是一个比较通用的搜索匹配,请按需重载此方法来编写自己的搜索逻辑,$where按自己的需求写即可
+     * 这里示例了所有的参数，所以比较复杂，实现上自己实现只需简单的几行即可
+     *
+     */
+    protected function selectpage()
+    {
+        //设置过滤方法
+        $this->request->filter(['strip_tags', 'htmlspecialchars']);
+
+        //搜索关键词,客户端输入以空格分开,这里接收为数组
+        $word = (array)$this->request->request("q_word/a");
+        //当前页
+        $page = $this->request->request("pageNumber");
+        //分页大小
+        $pagesize = $this->request->request("pageSize");
+        //搜索条件
+        $andor = $this->request->request("andOr", "and", "strtoupper");
+        //排序方式
+        $orderby = (array)$this->request->request("orderBy/a");
+        //显示的字段
+        $field = $this->request->request("showField");
+        //主键
+        $primarykey = $this->request->request("keyField");
+        //主键值
+        $primaryvalue = $this->request->request("keyValue");
+        //搜索字段
+        $searchfield = (array)$this->request->request("searchField/a");
+        //自定义搜索条件
+        $custom = (array)$this->request->request("custom/a");
+        //是否返回树形结构
+        $istree = $this->request->request("isTree", 0);
+        $ishtml = $this->request->request("isHtml", 0);
+        if ($istree) {
+            $word = [];
+            $pagesize = 99999;
+        }
+        $order = [];
+        foreach ($orderby as $k => $v) {
+            $order[$v[0]] = $v[1];
+        }
+        $field = $field ? $field : 'name';
+
+        //如果有primaryvalue,说明当前是初始化传值
+        if ($primaryvalue !== null) {
+            $where = [$primarykey => ['in', $primaryvalue]];
+            $pagesize = 99999;
+        } else {
+            $where = function ($query) use ($word, $andor, $field, $searchfield, $custom) {
+                $logic = $andor == 'AND' ? '&' : '|';
+                $searchfield = is_array($searchfield) ? implode($logic, $searchfield) : $searchfield;
+                $word = array_filter($word);
+                if ($word) {
+                    foreach ($word as $k => $v) {
+                        $query->where(str_replace(',', $logic, $searchfield), "like", "%{$v}%");
+                    }
+                }
+                if ($custom && is_array($custom)) {
+                    foreach ($custom as $k => $v) {
+                        if (is_array($v) && 2 == count($v)) {
+                            $query->where($k, trim($v[0]), $v[1]);
+                        } else {
+                            $query->where($k, '=', $v);
+                        }
+                    }
+                }
+            };
+        }
+        /*$adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds)) {
+            $this->model->where($this->dataLimitField, 'in', $adminIds);
+        }*/
+        $list = [];
+        $total = $this->modelClass->where($where)->count();
+        if ($total > 0) {
+            /*if (is_array($adminIds)) {
+                $this->model->where($this->dataLimitField, 'in', $adminIds);
+            }*/
+            $datalist = $this->modelClass->where($where)
+                ->order($order)
+                ->page($page, $pagesize)
+                ->field($this->selectpageFields)
+                ->select();
+            foreach ($datalist as $index => $item) {
+                unset($item['password'], $item['salt']);
+                $list[] = [
+                    $primarykey => isset($item[$primarykey]) ? $item[$primarykey] : '',
+                    $field      => isset($item[$field]) ? $item[$field] : '',
+                    'pid'       => isset($item['pid']) ? $item['pid'] : 0
+                ];
+            }
+            if ($istree && !$primaryvalue) {
+                $tree = \util\Tree::instance();
+                $tree->init(collection($list)->toArray(), 'pid');
+                $list = $tree->getTreeList($tree->getTreeArray(0), $field);
+                if (!$ishtml) {
+                    foreach ($list as &$item) {
+                        $item = str_replace('&nbsp;', ' ', $item);
+                    }
+                    unset($item);
+                }
+            }
+        }
+        //这里一定要返回有list这个字段,total是可选的,如果total<=list的数量,则会隐藏分页按钮
+        return json(['data' => $list, 'count' => $total]);
+    }
+
 }
