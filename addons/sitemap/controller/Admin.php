096a50813cefe1fe756ccdba7d71b42e29a40fc9
yzncms <530765310@qq.com>
2020-09-14 12:31:30 +0800

    网站地图模块修改为插件

---
+++ addons/sitemap/controller/Admin.php
@@ -0,0 +1,147 @@
+<?php
+// +----------------------------------------------------------------------
+// | Yzncms [ 御宅男工作室 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://yzncms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 御宅男 <530765310@qq.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | sitemap管理
+// +----------------------------------------------------------------------
+namespace addons\sitemap\Controller;
+
+use addons\sitemap\library\Sitemap as Sitemap_Class;
+use app\addons\util\Adminaddon;
+use think\Db;
+
+class Admin extends Adminaddon
+{
+    protected $filename = 'sitemap.xml';
+    protected $data     = [];
+    protected $directory;
+
+    protected function initialize()
+    {
+        parent::initialize();
+        $this->directory = (defined('IF_PUBLIC') ? ROOT_PATH . 'public/' : ROOT_PATH);
+    }
+
+    public function index()
+    {
+        if ($this->request->isPost()) {
+            $data        = $this->request->post();
+            $Sitemap     = new Sitemap_Class();
+            $rootUrl     = $this->request->domain();
+            $data['num'] = intval($data['num']);
+            $type        = isset($data['type']) ? intval($data['type']) : 1;
+
+            $item = $this->_sitemap_item($rootUrl, intval($data['index']['priority']), $data['index']['changefreq'], time());
+            $this->_add_data($item);
+
+            //栏目
+            $List = Db::name('Category')->where('status', 1)->order('id desc')->field('id,url')->select();
+            if (!empty($List)) {
+                foreach ($List as $vo) {
+                    $item = $this->_sitemap_item($rootUrl . url('cms/index/lists', ['catid' => $vo['id']]), intval($data['category']['priority']), $data['category']['changefreq'], time());
+                    $this->_add_data($item);
+                }
+            }
+
+            //单页
+            $List = Db::name('Page')->order('updatetime desc')->field('catid,updatetime')->select();
+            if (!empty($List)) {
+                foreach ($List as $vo) {
+                    $item = $this->_sitemap_item($rootUrl . url('cms/index/lists', ['catid' => $vo['catid']]), intval($data['content']['priority']), $data['content']['changefreq'], $v['updatetime']);
+                    $this->_add_data($item);
+                }
+            }
+
+            //列表
+            $modelList = cache('Model');
+            if (!empty($modelList)) {
+                $num    = 1;
+                $volist = [];
+                foreach ($modelList as $vo) {
+                    if ($vo['module'] == "cms") {
+                        $volist = Db::name($vo['tablename'])->where('status', 1)->order('updatetime desc')->field('id,catid,updatetime')->select();
+                        if (!empty($volist)) {
+                            foreach ($volist as $v) {
+                                $item = $this->_sitemap_item($rootUrl . url('cms/index/shows', ['catid' => $v['catid'], 'id' => $v['id']]), intval($data['content']['priority']), $data['content']['changefreq'], $v['updatetime']);
+                                $this->_add_data($item);
+                                $num++;
+                                if ($num >= $data['num']) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!$type) {
+                try {
+                    foreach ($this->data as $val) {
+                        $Sitemap->AddItem($val['loc'], $val['priority'], $val['changefreq'], $val['lastmod']);
+                    }
+                    $Sitemap->SaveToFile($this->directory . $this->filename);
+                } catch (\Exception $ex) {
+                    $this->error($ex->getMessage());
+                }
+            } else {
+                $str            = $this->_txt_format();
+                $this->filename = 'sitemap.txt';
+                @file_put_contents($this->directory . $this->filename, $str);
+            }
+            $this->success($this->filename . "文件已生成到运行根目录");
+
+        } else {
+
+            if (is_file($this->directory . 'sitemap.xml')) {
+                $make_xml_time = date('Y-m-d H:i:s', filemtime($this->directory . 'sitemap.xml'));
+                $this->assign('make_xml_time', $make_xml_time);
+            }
+            if (is_file($this->directory . 'sitemap.txt')) {
+                $make_txt_time = date('Y-m-d H:i:s', filemtime($this->directory . 'sitemap.txt'));
+                $this->assign('make_txt_time', $make_txt_time);
+            }
+            return $this->fetch();
+        }
+    }
+
+    /**
+     * 添加数据
+     */
+    private function _add_data($new_item)
+    {
+        $this->data[] = $new_item;
+    }
+
+    /**
+     * 生成txt格式
+     */
+    private function _txt_format()
+    {
+        $str = '';
+        foreach ($this->data as $val) {
+            $str .= $val['loc'] . PHP_EOL;
+        }
+        return $str;
+    }
+
+    /**
+     * 创建地图格式
+     */
+    private function _sitemap_item($loc, $priority = '', $changefreq = '', $lastmod = '')
+    {
+        $data               = array();
+        $data['loc']        = $loc;
+        $data['priority']   = $priority;
+        $data['changefreq'] = $changefreq;
+        $data['lastmod']    = $lastmod;
+        return $data;
+    }
+
+}
---
+++ addons/sitemap/library/Sitemap.php
@@ -0,0 +1,175 @@
+<?php
+// +----------------------------------------------------------------------
+// | Yzncms [ 御宅男工作室 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://yzncms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 御宅男 <530765310@qq.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | sitemap类
+// +----------------------------------------------------------------------
+namespace addons\sitemap\library;
+
+class Sitemap
+{
+//类定义开始
+
+    private $config = array(
+        'encoding' => 'UTF-8',
+        'ver'      => '1.0',
+    );
+    private $content = '';
+    // Items部分
+    private $items = array();
+
+    public function __get($name)
+    {
+        if (isset($this->config[$name])) {
+            return $this->config[$name];
+        }
+        return null;
+    }
+
+    public function __set($name, $value)
+    {
+        if (isset($this->config[$name])) {
+            $this->config[$name] = $value;
+        }
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->config[$name]);
+    }
+
+    public function content($name)
+    {
+        if (empty($this->content)) {
+            $this->Build();
+        }
+
+        $this->content;
+    }
+
+    /**
+     * 架构函数
+     * @access public
+     * @param array $config  上传参数
+     */
+    public function __construct()
+    {
+
+    }
+
+    /*     * *********************************************************************** */
+
+    // 函数名: AddItem
+    // 功能: 添加一个节点
+    //$changefreq | always 经常,hourly 每小时,daily 每天,weekly 每周,monthly 每月,yearly 每年,never 从不
+    //$mobile | mobile 跳转适配, htmladapt 代码适应,  pc,mobile 自适应
+    /*     * *********************************************************************** */
+
+    public function AddItem($loc, $priority, $changefreq = 'Always', $time = 0, $mobile = "pc,mobile")
+    {
+        $arr = array(
+            '1.0',
+            '0.9',
+            '0.8',
+            '0.7',
+            '0.6',
+            '0.5',
+        );
+        $this->items[] = array(
+            'loc'        => $loc,
+            'priority'   => $arr[$priority],
+            'lastmod'    => $time ? (is_numeric($time) ? date('Y-m-d H:i:s', $time) : $time) : date('Y-m-d H:i:s', time()),
+            'changefreq' => $changefreq,
+            'mobile'     => $mobile,
+        );
+    }
+
+    /*     * *********************************************************************** */
+
+    // 函数名: Build
+    // 功能: 生成sitemap xml文件内容
+    /*     * *********************************************************************** */
+    public function Build()
+    {
+        $s = "<?xml version='1.0' encoding='{$this->encoding}'?>\r\n";
+        /* $s .= "<?xml-stylesheet type='text/xsl' href='sitemap.xsl'?>\r\n";*/
+        $s .= "\t<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>\r\n";
+        // items
+        for ($i = 0; $i < count($this->items); $i++) {
+            $s .= "\t\t<url>\n";
+            $s .= "\t\t\t<loc>{$this->items[$i]['loc']}</loc>\r\n";
+            $s .= "\t\t\t<priority>{$this->items[$i]['priority']}</priority>\r\n";
+            $s .= "\t\t\t<lastmod>{$this->items[$i]['lastmod']}</lastmod>\r\n";
+            $s .= "\t\t\t<changefreq>{$this->items[$i]['changefreq']}</changefreq>\r\n";
+            /*$s .= "\t\t\t<mobile:mobile type=\"{$this->items[$i]['mobile']}\"/>\r\n";*/
+            $s .= "\t\t</url>\n";
+        }
+        // close
+        $s .= "\t</urlset>";
+        $this->content = $s;
+    }
+
+    /*     * *********************************************************************** */
+
+    // 函数名: Show
+    // 功能: 将产生的sitemap内容直接打印输出
+    /*     * *********************************************************************** */
+    public function Show()
+    {
+        if (empty($this->content)) {
+            $this->Build();
+        }
+
+        header("Content-Type: text/xml; charset=utf-8");
+        echo ($this->content);
+    }
+
+    /*     * *********************************************************************** */
+
+    // 函数名: SaveToFile
+    // 功能: 将产生的sitemap 内容保存到文件
+    // 参数: $fname 要保存的文件名
+    /*     * *********************************************************************** */
+    public function SaveToFile($fname)
+    {
+        if (empty($this->content)) {
+            $this->Build();
+        }
+
+        $handle = fopen($fname, 'w+');
+        if ($handle === false) {
+            return false;
+        }
+
+        fwrite($handle, $this->content);
+        fclose($handle);
+    }
+
+    /*     * *********************************************************************** */
+
+    // 函数名: getFile
+    // 功能: 从文件中获取输出
+    // 参数: $fname 文件名
+    /*     * *********************************************************************** */
+    public function getFile($fname)
+    {
+        $handle = fopen($fname, 'r');
+        if ($handle === false) {
+            return false;
+        }
+
+        while (!feof($handle)) {
+            echo fgets($handle);
+        }
+        fclose($handle);
+    }
+
+}
