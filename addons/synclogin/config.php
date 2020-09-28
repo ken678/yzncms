29b0d2c663c31c46e086cf4f0f6b819394301682
yzncms <530765310@qq.com>
2020-09-28 13:51:37 +0800

    ..

--- addons/synclogin/config.php
+++ addons/synclogin/config.php
@@ -1,70 +1,70 @@
 <?php
 
 return array(
-    'type' => array(
-        'title' => '开启同步登陆：',
-        'type' => 'checkbox',
+    'type'         => array(
+        'title'   => '开启同步登陆：',
+        'type'    => 'checkbox',
         'options' => array(
-            'Qq' => 'Qq',
-            'Sina' => 'Sina',
+            'Qq'     => 'Qq',
+            'Sina'   => 'Sina',
             'Weixin' => 'Weixin',
         ),
     ),
-    'bind' => array( //配置在表单中的键名 ,这个会是config[title]
-        'title' => '是否开启帐号绑定：', //表单的文字
-        'type' => 'radio', //表单的类型：text、textarea、checkbox、radio、select等
+    'bind'         => array( //配置在表单中的键名 ,这个会是config[title]
+        'title'   => '是否开启帐号绑定：', //表单的文字
+        'type'    => 'radio', //表单的类型：text、textarea、checkbox、radio、select等
         'options' => array(
             '1' => '是',
             '0' => '否',
         ),
-        'value' => '0',
-        'tip' => '不开启则跳过与本地帐号绑定过程，建议审核时关闭绑定。',
+        'value'   => '0',
+        'tip'     => '不开启则跳过与本地帐号绑定过程，建议审核时关闭绑定。',
     ),
-    'QqKEY' => array(
+    'QqKEY'        => array(
         'title' => 'QQ互联APP ID：',
-        'type' => 'text',
+        'type'  => 'text',
         'value' => '',
-        'tip' => '申请地址：http://connect.qq.com',
+        'tip'   => '申请地址：http://connect.qq.com',
     ),
-    'QqSecret' => array(
+    'QqSecret'     => array(
         'title' => 'QQ互联APP KEY：',
-        'type' => 'text',
+        'type'  => 'text',
         'value' => '',
-        'tip' => '申请地址：http://connect.qq.com',
+        'tip'   => '申请地址：http://connect.qq.com',
     ),
 
-    'SinaKEY' => array(
+    'SinaKEY'      => array(
         'title' => '新浪App Key：',
-        'type' => 'text',
+        'type'  => 'text',
         'value' => '',
-        'tip' => '申请地址：http://open.weibo.com/',
+        'tip'   => '申请地址：http://open.weibo.com/',
     ),
-    'SinaSecret' => array(
+    'SinaSecret'   => array(
         'title' => '新浪App Sercet：',
-        'type' => 'text',
+        'type'  => 'text',
         'value' => '',
-        'tip' => '申请地址：http://open.weibo.com/',
+        'tip'   => '申请地址：http://open.weibo.com/',
     ),
 
-    'WeixinKEY' => array(
+    'WeixinKEY'    => array(
         'title' => '微信App Key：',
-        'type' => 'text',
+        'type'  => 'text',
         'value' => '',
-        'tip' => '电脑扫码申请地址：https://open.weixin.qq.com/',
+        'tip'   => '电脑扫码申请地址：https://open.weixin.qq.com/',
     ),
     'WeixinSecret' => array(
         'title' => '微信App Sercet：',
-        'type' => 'text',
+        'type'  => 'text',
         'value' => '',
-        'tip' => '公众号申请地址：https://mp.weixin.qq.com/',
+        'tip'   => '公众号申请地址：https://mp.weixin.qq.com/',
     ),
-    'display' => array(
-        'title' => '微信登录选择方式',
-        'type' => 'radio',
+    'display'      => array(
+        'title'   => '微信登录选择方式',
+        'type'    => 'radio',
         'options' => array(
             '1' => '电脑扫码登录',
             '0' => '公众号登录',
         ),
-        'value' => '1',
+        'value'   => '1',
     ),
 );
