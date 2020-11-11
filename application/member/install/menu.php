3aef8b8bbfa92b5993349fe323c0b001eb2f866d
yzncms <530765310@qq.com>
2020-11-11 15:23:53 +0800

    图标优化

--- application/member/install/menu.php
+++ application/member/install/menu.php
@@ -2,66 +2,66 @@
 return [
     [
         //父菜单ID，NULL或者不写系统默认，0为顶级菜单
-        "parentid" => 0,
+        "parentid"  => 0,
         //地址，[模块/]控制器/方法
-        "route" => "member/member/index1",
+        "route"     => "member/member/index1",
         //类型，1：权限认证+菜单，0：只作为菜单
-        "type" => 0,
+        "type"      => 0,
         //状态，1是显示，0不显示（需要参数的，建议不显示，例如编辑,删除等操作）
-        "status" => 1,
+        "status"    => 1,
         //名称
-        "name" => "会员",
+        "name"      => "会员",
         //图标
-        "icon" => "icon-people",
+        "icon"      => "icon-people",
         //备注
-        "remark" => "",
+        "remark"    => "",
         //排序
         "listorder" => 4,
         //子菜单列表
-        "child" => [
+        "child"     => [
             [
-                "route" => "member/member/index",
-                "type" => 1,
+                "route"  => "member/member/index",
+                "type"   => 1,
                 "status" => 1,
-                "name" => "会员管理",
-                "icon" => "icon-people",
-                "child" => [
+                "name"   => "会员管理",
+                "icon"   => "icon-people",
+                "child"  => [
                     [
-                        "route" => "member/setting/setting",
-                        "type" => 1,
+                        "route"  => "member/setting/setting",
+                        "type"   => 1,
                         "status" => 1,
-                        "name" => "会员设置",
-                        "icon" => "icon-setup",
+                        "name"   => "会员设置",
+                        "icon"   => "icon-setup",
                     ],
                     [
-                        "route" => "member/member/manage",
-                        "type" => 1,
+                        "route"  => "member/member/manage",
+                        "type"   => 1,
                         "status" => 1,
-                        "name" => "会员管理",
-                        "icon" => "icon-huiyuan",
+                        "name"   => "会员管理",
+                        "icon"   => "icon-user-settings-line",
                     ],
                     [
-                        "route" => "member/member/userverify",
-                        "type" => 1,
+                        "route"  => "member/member/userverify",
+                        "type"   => 1,
                         "status" => 1,
-                        "name" => "审核会员",
-                        "icon" => "icon-shenhe",
+                        "name"   => "审核会员",
+                        "icon"   => "icon-shenhe",
                     ],
                 ],
             ],
             [
-                "route" => "member/group/index1",
-                "type" => 1,
+                "route"  => "member/group/index1",
+                "type"   => 1,
                 "status" => 1,
-                "name" => "会员组",
-                "icon" => "icon-huiyuan2",
-                "child" => [
+                "name"   => "会员组",
+                "icon"   => "icon-group-line",
+                "child"  => [
                     [
-                        "route" => "member/group/index",
-                        "type" => 1,
+                        "route"  => "member/group/index",
+                        "type"   => 1,
                         "status" => 1,
-                        "name" => "会员组管理",
-                        "icon" => "icon-huiyuan2",
+                        "name"   => "会员组管理",
+                        "icon"   => "icon-user-settings-line",
                     ],
                 ],
             ],
