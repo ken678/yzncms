632133096b66774a0971a49d69571f2ad4f35510
yzncms <530765310@qq.com>
2020-11-16 15:20:34 +0800

    暂时简单处理模型字段的默认值

--- application/cms/model/ModelField.php
+++ application/cms/model/ModelField.php
@@ -49,10 +49,11 @@ class ModelField extends Modelbase
         if ($data['ifrequire'] && !$data['isadd']) {
             throw new \Exception('必填字段不可以隐藏！');
         }
+        $dfaultvalue = empty($data['setting']['value']) ? $this->setDfaultValue($data) : $data['setting']['value'];
         //先将字段存在设置的主表或附表里面 再将数据存入ModelField
         $sql = <<<EOF
             ALTER TABLE `{$tablename}`
-            ADD COLUMN `{$data['name']}` {$data['setting']['define']} COMMENT '{$data['title']}';
+            ADD COLUMN `{$data['name']}` {$data['setting']['define']} DEFAULT '{$dfaultvalue}' COMMENT '{$data['title']}';
 EOF;
         Db::execute($sql);
         $fieldInfo = Db::name('field_type')->where('name', $data['type'])->field('ifoption,ifstring')->find();
@@ -114,9 +115,10 @@ EOF;
         if ($data['ifrequire'] && !$data['isadd']) {
             throw new \Exception('必填字段不可以隐藏！');
         }
-        $sql = <<<EOF
+        $dfaultvalue = empty($data['setting']['value']) ? $this->setDfaultValue($data) : $data['setting']['value'];
+        $sql         = <<<EOF
             ALTER TABLE `{$tablename}`
-            CHANGE COLUMN `{$info['name']}` `{$data['name']}` {$data['setting']['define']} COMMENT '{$data['title']}';
+            CHANGE COLUMN `{$info['name']}` `{$data['name']}` {$data['setting']['define']} DEFAULT '{$dfaultvalue}' COMMENT '{$data['title']}';
 EOF;
         try {
             Db::execute($sql);
@@ -166,6 +168,16 @@ EOF;
         Db::execute($sql);
         self::get($fieldid)->delete();
         return true;
+    }
+
+    //设置默认值var为空，int为0 //TODO 后续需要调整为生成sql 目前暂时先这样简单处理
+    protected function setDfaultValue($data)
+    {
+        if (false !== strstr(strtolower($data['setting']['define']), 'int')) {
+            return 0;
+        } else {
+            return null;
+        }
     }
 
     /**
