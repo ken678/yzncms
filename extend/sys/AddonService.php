f5756a1b320162d682dac468a4b45cb17709e406
yzncms <530765310@qq.com>
2020-11-20 08:58:23 +0800

    更新

--- extend/sys/AddonService.php
+++ extend/sys/AddonService.php
@@ -16,6 +16,8 @@
 // +----------------------------------------------------------------------
 namespace sys;
 
+use PhpZip\Exception\ZipException;
+use PhpZip\ZipFile;
 use think\Exception;
 use util\Sql;
 
@@ -207,47 +209,60 @@ class AddonService
      */
     public static function local($file)
     {
-        $addonTmpDir = ROOT_PATH . 'runtime' . DS . 'addons' . DS;
-        if (!is_dir($addonTmpDir)) {
-            @mkdir($addonTmpDir, 0755, true);
+        $addonsTempDir = self::getAddonsBackupDir();
+        if (!$file || !$file instanceof \think\File) {
+            throw new Exception('没有文件上传或服务器上传限制');
         }
-        $info = $file->rule('uniqid')->validate(['size' => 10240000, 'ext' => 'zip'])->move($addonTmpDir);
-        if ($info) {
-            $tmpName     = substr($info->getFilename(), 0, stripos($info->getFilename(), '.'));
-            $tmpAddonDir = ADDON_PATH . $tmpName . DS;
-            $tmpFile     = $addonTmpDir . $info->getSaveName();
-            try {
-                $zip = new \util\PclZip($tmpFile);
-                $zip->extract(PCLZIP_OPT_PATH, $tmpAddonDir);
-                unset($info);
-                @unlink($tmpFile);
-
-                $infoFile = $tmpAddonDir . 'info.ini';
-                if (!is_file($infoFile)) {
-                    throw new Exception('插件信息不存在');
-                }
-                $config = parse_ini_file($infoFile, true, INI_SCANNER_TYPED) ?: [];
-                $name   = isset($config['name']) ? $config['name'] : '';
-                if (!$name) {
-                    throw new Exception('插件配置信息不正确');
-                }
-                if (!preg_match("/^[a-zA-Z0-9]+$/", $name)) {
-                    throw new Exception('插件名称不正确');
-                }
-                $newAddonDir = ADDON_PATH . $name . DS;
-                if (is_dir($newAddonDir)) {
-                    throw new Exception('上传的插件已经存在');
-                }
-                //重命名插件文件夹
-                rename($tmpAddonDir, $newAddonDir);
-            } catch (Exception $e) {
-                unset($info);
-                @unlink($tmpFile);
-                @\util\File::del_dir($tmpAddonDir);
-                throw new Exception($e->getMessage());
-            }
-        } else {
+        $uploadFile = $file->rule('uniqid')->validate(['size' => 102400000, 'ext' => 'zip'])->move($addonsTempDir);
+        if (!$uploadFile) {
+            // 上传失败获取错误信息
             throw new Exception($file->getError());
+        }
+        $tmpFile = $addonsTempDir . $uploadFile->getSaveName();
+        $info    = [];
+        $zip     = new ZipFile();
+        try {
+            // 打开插件压缩包
+            try {
+                $zip->openFile($tmpFile);
+            } catch (ZipException $e) {
+                @unlink($tmpFile);
+                throw new Exception('无法打开压缩文件');
+            }
+
+            $config = self::getInfoIni($zip);
+            // 判断插件标识
+            $name = isset($config['name']) ? $config['name'] : '';
+            if (!$name) {
+                throw new Exception('插件info.ini文件不正确');
+            }
+
+            // 判断插件是否存在
+            if (!preg_match("/^[a-zA-Z0-9]+$/", $name)) {
+                throw new Exception('插件名称不正确');
+            }
+
+            // 判断新插件是否存在
+            $newAddonDir = self::getAddonDir($name);
+            if (is_dir($newAddonDir)) {
+                throw new Exception('插件已经存在');
+            }
+
+            //创建插件目录
+            @mkdir($newAddonDir, 0755, true);
+            // 解压到插件目录
+            try {
+                $zip->extractTo($newAddonDir);
+            } catch (ZipException $e) {
+                @unlink($newAddonDir);
+                throw new Exception('无法解压缩文件');
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        } finally {
+            $zip->close();
+            unset($uploadFile);
+            @unlink($tmpFile);
         }
     }
 
@@ -300,6 +315,46 @@ class AddonService
             }
         }
         return true;
+    }
+
+    /**
+     * 匹配配置文件中info信息
+     * @param ZipFile $zip
+     * @return array|false
+     * @throws Exception
+     */
+    protected static function getInfoIni($zip)
+    {
+        $config = [];
+        // 读取插件信息
+        try {
+            $info   = $zip->getEntryContents('info.ini');
+            $config = parse_ini_string($info);
+        } catch (ZipException $e) {
+            throw new Exception('无法解压缩文件');
+        }
+        return $config;
+    }
+
+    /**
+     * 获取插件备份目录
+     */
+    public static function getAddonsBackupDir()
+    {
+        $dir = ROOT_PATH . 'runtime' . DS . 'addons' . DS;
+        if (!is_dir($dir)) {
+            @mkdir($dir, 0755, true);
+        }
+        return $dir;
+    }
+
+    /**
+     * 获取指定插件的目录
+     */
+    public static function getAddonDir($name)
+    {
+        $dir = ADDON_PATH . $name . DS;
+        return $dir;
     }
 
     /**
