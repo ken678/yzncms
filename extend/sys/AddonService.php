837264335e58e215329d1f8e06110a3f8476a0df
yzncms <530765310@qq.com>
2020-11-20 10:33:03 +0800

    更新

--- extend/sys/AddonService.php
+++ extend/sys/AddonService.php
@@ -66,7 +66,7 @@ class AddonService
                 $addon->install();
             }
             if (isset($info['has_adminlist']) && $info['has_adminlist']) {
-                $admin_list = $addon->admin_list;
+                $admin_list = property_exists($addon, 'admin_list') ? $addon->admin_list : '';
                 //添加菜单
                 model('admin/Menu')->addAddonMenu($info, $admin_list);
             }
