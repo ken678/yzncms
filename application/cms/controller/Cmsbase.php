10c957ddd740bdba7a63aa0801324c5c4f020aea
yzncms <530765310@qq.com>
2020-11-30 09:41:25 +0800

    更新

--- application/cms/controller/Cmsbase.php
+++ application/cms/controller/Cmsbase.php
@@ -20,12 +20,15 @@ class Cmsbase extends Homebase
 {
     //CMS模型相关配置
     protected $cmsConfig = [];
+
     //初始化
     protected function initialize()
     {
         parent::initialize();
+        $siteurl         = url('cms/index/index', '', true, false);
         $this->cmsConfig = cache("Cms_Config");
         $this->assign("cms_config", $this->cmsConfig);
+        $this->assign("siteurl", $siteurl);
         if (!$this->cmsConfig['web_site_status']) {
             $this->error("站点已经关闭，请稍后访问~");
         }
