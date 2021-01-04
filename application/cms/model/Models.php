6a3a46209b10873a50107524f5a04983add4d86b
yzncms <530765310@qq.com>
2021-01-04 11:41:07 +0800

    更新

--- application/cms/model/Models.php
+++ application/cms/model/Models.php
@@ -173,7 +173,7 @@ EOF;
             $sql = <<<EOF
                 CREATE TABLE `{$table}{$this->ext_table}` (
                 `did` mediumint(8) unsigned NOT NULL DEFAULT '0',
-                `content` text COLLATE utf8_unicode_ci COMMENT '内容',
+                `content` mediumtext COLLATE utf8_unicode_ci COMMENT '内容',
                 PRIMARY KEY (`did`)
                 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='{$data['name']}模型表';
 EOF;
