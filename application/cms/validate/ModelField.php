302deb1b8cefcd0c08bbb44b20e4cb946f3fc17c
yzncms <530765310@qq.com>
2021-01-08 17:00:42 +0800

    更新

--- application/cms/validate/ModelField.php
+++ application/cms/validate/ModelField.php
@@ -20,27 +20,18 @@ class ModelField extends Validate
 {
     //定义验证规则
     protected $rule = [
-        'name'           => 'require|regex:/^[a-zA-Z][A-Za-z0-9]+$/',
-        'title'          => 'require|chsAlpha',
-        'type'           => 'require|alphaDash',
-        'setting.define' => 'require',
-        'ifsystem'       => 'in:0,1',
-        'ifrequire'      => 'in:0,1',
-        'ifsearch'       => 'in:0,1',
-        'status'         => 'in:0,1',
+        'name'                => 'require|regex:/^[a-zA-Z][A-Za-z0-9]+$/',
+        'title|字段标题'          => 'require|chsDash',
+        'type|字段类型'           => 'require|alphaDash',
+        'setting.define|字段定义' => 'require',
+        'ifsystem|主表字段'       => 'in:0,1',
+        'ifrequire|是否必填格'     => 'in:0,1',
+        'ifsearch|是否显示搜索'     => 'in:0,1',
+        'status|字段状态'         => 'in:0,1',
     ];
     //定义验证提示
     protected $message = [
-        'name.require'           => '字段名称不得为空',
-        'name.regex'             => '字段名称只能为字母和数字，并且仅能字母开头',
-        'title.require'          => '字段标题不得为空',
-        'title.chsAlpha'         => '字段标题只能为只能是汉字和字母',
-        'type.require'           => '字段类型不得为空',
-        'type.alphaDash'         => '字段类型格式错误',
-        'setting.define.require' => '字段定义不得为空',
-        'ifsystem.in'            => '主表字段格式错误',
-        'ifrequire.in'           => '是否必填格式错误',
-        'ifsearch.in'            => '是否显示搜索格式错误',
-        'status.in'              => '字段状态格式错误',
+        'name.require' => '字段名称不得为空',
+        'name.regex'   => '字段名称只能为字母和数字，并且仅能字母开头',
     ];
 }
