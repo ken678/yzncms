45e31b1e1843573b4fb93cb8601a84b5df831d95
yzncms <530765310@qq.com>
2021-01-13 17:09:25 +0800

    更新

--- application/cms/install/route_cms.php
+++ application/cms/install/route_cms.php
@@ -18,8 +18,8 @@ Route::group('/', function () {
     Route::rule('lists/:catid/[:condition]', 'cms/index/lists')->pattern(['catid' => '\d+', 'condition' => '[0-9_&=a-zA-Z]+']);
     Route::rule('shows/:catid/:id', 'cms/index/shows')->pattern(['catid' => '\d+', 'id' => '\d+']);
     if (isset(cache("Cms_Config")['site_url_mode']) && 2 == cache("Cms_Config")['site_url_mode']) {
-        Route::rule('lists/:catdir/[:condition]', 'cms/index/lists')->pattern(['catdir' => '[a-zA-Z]+', 'condition' => '[0-9_&=a-zA-Z]+']);
-        Route::rule('shows/:catdir/:id', 'cms/index/shows')->pattern(['catdir' => '[a-zA-Z]+', 'id' => '\d+']);
+        Route::rule('c/:catdir/[:condition]', 'cms/index/lists')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'condition' => '[0-9_&=a-zA-Z]+']);
+        Route::rule('d/:catdir/:id', 'cms/index/shows')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'id' => '\d+']);
     }
     Route::rule('tag/[:tag]', 'cms/index/tags');
     Route::rule('search', 'cms/index/search');
