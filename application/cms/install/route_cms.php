b1f9be8795c4f92b57731b8958241e4e536038ff
ken678 <530765310@qq.com>
2021-01-13 21:47:49 +0800

    更新

--- application/cms/install/route_cms.php
+++ application/cms/install/route_cms.php
@@ -18,8 +18,9 @@ Route::group('/', function () {
     Route::rule('lists/:catid/[:condition]', 'cms/index/lists')->pattern(['catid' => '\d+', 'condition' => '[0-9_&=a-zA-Z]+']);
     Route::rule('shows/:catid/:id', 'cms/index/shows')->pattern(['catid' => '\d+', 'id' => '\d+']);
     if (isset(cache("Cms_Config")['site_url_mode']) && 2 == cache("Cms_Config")['site_url_mode']) {
-        Route::rule('c/:catdir/[:condition]', 'cms/index/lists')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'condition' => '[0-9_&=a-zA-Z]+']);
+        //Route::rule('admin', 'admin/index/login');//如去除c/ d/ 需要解开此注释
         Route::rule('d/:catdir/:id', 'cms/index/shows')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'id' => '\d+']);
+        Route::rule('c/:catdir/[:condition]', 'cms/index/lists')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'condition' => '[0-9_&=a-zA-Z]+']);
     }
     Route::rule('tag/[:tag]', 'cms/index/tags');
     Route::rule('search', 'cms/index/search');
