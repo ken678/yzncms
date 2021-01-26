c59622d44d43371a764ec4aabadd81f8cae07279
yzncms <530765310@qq.com>
2021-01-26 09:30:29 +0800

    修正标识路由

--- application/cms/install/route_cms.php
+++ application/cms/install/route_cms.php
@@ -17,11 +17,11 @@ Route::group('/', function () {
     Route::rule('index', 'cms/index/index');
     Route::rule('lists/:catid/[:condition]', 'cms/index/lists')->pattern(['catid' => '\d+', 'condition' => '[0-9_&=a-zA-Z]+']);
     Route::rule('shows/:catid/:id', 'cms/index/shows')->pattern(['catid' => '\d+', 'id' => '\d+']);
+    Route::rule('tag/[:tag]', 'cms/index/tags');
+    Route::rule('search', 'cms/index/search');
     if (isset(cache("Cms_Config")['site_url_mode']) && 2 == cache("Cms_Config")['site_url_mode']) {
         //Route::rule('admin', 'admin/index/login');//如去除c/ d/ 需要解开此注释
         Route::rule('d/:catdir/:id', 'cms/index/shows')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'id' => '\d+']);
         Route::rule('c/:catdir/[:condition]', 'cms/index/lists')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'condition' => '[0-9_&=a-zA-Z]+']);
     }
-    Route::rule('tag/[:tag]', 'cms/index/tags');
-    Route::rule('search', 'cms/index/search');
 });
