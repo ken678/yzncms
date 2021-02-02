541ef6c0286dc53abef3fb4f1cf92289391f82df
yzncms <530765310@qq.com>
2021-02-02 08:51:01 +0800

    更新

--- addons/loginbg/config.php
+++ addons/loginbg/config.php
@@ -1,11 +1,14 @@
 <?php
-[
-    'name'    => 'mode',
-    'title'   => '模式',
-    'type'    => 'radio',
-    'content' => [
-        'random' => '每次随机',
-        'daily'  => '每日切换',
-    ],
-    'value'   => 'daily',
-],
+return array(
+    array(
+        'name'    => 'mode',
+        'title'   => '模式',
+        'type'    => 'radio',
+        'options' => array(
+            'random' => '每次随机',
+            'daily'  => '每日切换',
+        ),
+        'value'   => 'daily',
+        'tip'     => '随机切换只支持七天内图片',
+    ),
+);
