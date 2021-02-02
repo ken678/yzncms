33817a449f9dd7ba5092d2f555a6ba2672b37f83
yzncms <530765310@qq.com>
2021-02-02 08:47:03 +0800

    更新

---
+++ addons/loginbg/Loginbg.php
@@ -0,0 +1,44 @@
+<?php
+// +----------------------------------------------------------------------
+// | Yzncms [ 御宅男工作室 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://yzncms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 御宅男 <530765310@qq.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | 后台登录背景图插件
+// +----------------------------------------------------------------------
+namespace addons\loginbg;
+
+use sys\Addons;
+
+class Loginbg extends Addons
+{
+    //安装
+    public function install()
+    {
+        return true;
+    }
+
+    //卸载
+    public function uninstall()
+    {
+        return true;
+    }
+
+    public function adminLoginStyle()
+    {
+        //$config = $this->getAddonConfig();
+        $gettime     = 6;
+        $json_string = file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx=' . $gettime . '&n=1');
+        $data        = json_decode($json_string);
+        $background  = "https://www.bing.com" . $data->{"images"}[0]->{"urlbase"} . "_1920x1080.jpg";
+        $this->assign('background', $background);
+        return $this->fetch('loginbg');
+    }
+
+}
---
+++ addons/loginbg/config.php
@@ -0,0 +1,11 @@
+<?php
+[
+    'name'    => 'mode',
+    'title'   => '模式',
+    'type'    => 'radio',
+    'content' => [
+        'random' => '每次随机',
+        'daily'  => '每日切换',
+    ],
+    'value'   => 'daily',
+],
