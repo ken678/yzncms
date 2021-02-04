ca3ec077894e775ca2b79fbedc6e4d4f417a56bf
yzncms <530765310@qq.com>
2021-02-04 15:14:39 +0800

    更新

--- addons/address/Address.php
+++ addons/address/Address.php
@@ -30,4 +30,23 @@ class Address extends Addons
         return true;
     }
 
+    public function showMap($params)
+    {
+        $config = $this->getAddonConfig();
+        if (!empty($config['appkey'])) {
+            $params['appkey']     = $config['appkey'];
+            $params['width']      = isset($params['width']) ? $params['width'] : 800;
+            $params['height']     = isset($params['height']) ? $params['height'] : 500;
+            $params['point']      = isset($params['point']) ? $params['point'] : 18;
+            $params['title']      = isset($params['title']) ? $params['title'] : '某某网络科技有限公司';
+            $params['content']    = isset($params['content']) ? $params['content'] : '公司地址：江苏省苏州市吴中区某某工业园一区<br/>手机：158-88888888<br/>邮箱：admin@admin.com';
+            $params['coordinate'] = (isset($params['coordinate']) && false !== strpos($params['coordinate'], ',')) ? explode(",", $params['coordinate']) : ['116.403874', '39.914889'];
+            $this->assign('params', $params);
+            return $this->fetch('map');
+        } else {
+            echo '<b>未设置百度地图秘钥，请先现在插件后台设置！</b>';
+        }
+
+    }
+
 }
---
+++ addons/address/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    array(
+        'name'  => 'appkey',
+        'title' => '百度地图秘钥',
+        'type'  => 'text',
+        'value' => '',
+        'tip'   => '请前往<a href="http://lbsyun.baidu.com/apiconsole/key?application=key" target="_blank">百度地图开放平台</a>获取秘钥',
+    ),
+);
