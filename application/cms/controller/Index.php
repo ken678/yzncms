a9dcd7bb84d87a494978a4a203dba5458a6d98d3
yzncms <530765310@qq.com>
2021-02-22 08:54:00 +0800

    管理员可以查看未审核文章

--- application/cms/controller/Index.php
+++ application/cms/controller/Index.php
@@ -134,8 +134,8 @@ class Index extends Cmsbase
         Db::name($modelInfo['tablename'])->where('id', $id)->setInc('hits');
         //内容所有字段
         $ifcache = $this->cmsConfig['site_cache_time'] ? $this->cmsConfig['site_cache_time'] : false;
-        $info    = $this->Cms_Model->getContent($modelid, "id='" . $id . "' and status='1'", true, '*', '', $ifcache);
-        if (empty($info)) {
+        $info    = $this->Cms_Model->getContent($modelid, "id={$id}", true, '*', '', $ifcache);
+        if (!$info || ($info['status'] !== 1 && !\app\admin\service\User::instance()->isLogin())) {
             throw new \think\Exception('内容不存在或未审核!', 404);
         }
         //栏目扩展配置信息
