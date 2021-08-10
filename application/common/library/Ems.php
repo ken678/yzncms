10fd3c08974b6e9868272c914fa0814bdea7166a
ken678 <530765310@qq.com>
2021-08-10 16:24:48 +0800

    ..

---
+++ application/common/library/Ems.php
@@ -0,0 +1,138 @@
+<?php
+// +----------------------------------------------------------------------
+// | Yzncms [ 御宅男工作室 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://yzncms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 御宅男 <530765310@qq.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | 邮箱验证码类
+// +----------------------------------------------------------------------
+namespace app\common\library;
+
+use app\common\model\Ems as EmsModel;
+
+class Ems
+{
+    /**
+     * 验证码有效时长
+     * @var int
+     */
+    protected static $expire = 120;
+
+    /**
+     * 最大允许检测的次数
+     * @var int
+     */
+    protected static $maxCheckNums = 10;
+
+    /**
+     * 获取最后一次邮箱发送的数据
+     *
+     * @param   int    $email 邮箱
+     * @param   string $event 事件
+     * @return  Ems
+     */
+    public static function get($email, $event = 'default')
+    {
+        $ems = EmsModel::where(['email' => $email, 'event' => $event])
+            ->order('id', 'DESC')
+            ->find();
+        hook('ems_get', $ems);
+        return $ems ? $ems : null;
+    }
+
+    /**
+     * 发送验证码
+     *
+     * @param   int    $email 邮箱
+     * @param   int    $code  验证码,为空时将自动生成4位数字
+     * @param   string $event 事件
+     * @return  boolean
+     */
+    public static function send($email, $code = null, $event = 'default')
+    {
+        $code   = is_null($code) ? mt_rand(1000, 9999) : $code;
+        $time   = time();
+        $ems    = EmsModel::create(['event' => $event, 'email' => $email, 'code' => $code, 'create_time' => $time]);
+        $result = hook('ems_send', $ems, true, true);
+        if (!$result) {
+            $ems->delete();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 发送通知
+     *
+     * @param   mixed  $email    邮箱,多个以,分隔
+     * @param   string $msg      消息内容
+     * @param   string $template 消息模板
+     * @return  boolean
+     */
+    public static function notice($email, $msg = '', $template = null)
+    {
+        $params = [
+            'email'    => $email,
+            'msg'      => $msg,
+            'template' => $template,
+        ];
+        $result = hook('ems_notice', $params, true, true);
+        return $result ? true : false;
+    }
+
+    /**
+     * 校验验证码
+     *
+     * @param   int    $email 邮箱
+     * @param   int    $code  验证码
+     * @param   string $event 事件
+     * @return  boolean
+     */
+    public static function check($email, $code, $event = 'default')
+    {
+        $time = time() - self::$expire;
+        $ems  = EmsModel::where(['email' => $email, 'event' => $event])
+            ->order('id', 'DESC')
+            ->find();
+        if ($ems) {
+            if ($ems['create_time'] > $time && $ems['times'] <= self::$maxCheckNums) {
+                $correct = $code == $ems['code'];
+                if (!$correct) {
+                    $ems->times = $ems->times + 1;
+                    $ems->save();
+                    return false;
+                } else {
+                    $result = hook('ems_check', $ems, true, true);
+                    return $result;
+                }
+            } else {
+                // 过期则清空该邮箱验证码
+                self::flush($email, $event);
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 清空指定邮箱验证码
+     *
+     * @param   int    $email 邮箱
+     * @param   string $event 事件
+     * @return  boolean
+     */
+    public static function flush($email, $event = 'default')
+    {
+        EmsModel::where(['email' => $email, 'event' => $event])->delete();
+        hook('ems_flush');
+        return true;
+    }
+
+}
--- application/common/model/Ems.php
+++ application/common/model/Ems.php
@@ -1,125 +1,20 @@
 <?php
-// +----------------------------------------------------------------------
-// | Yzncms [ 御宅男工作室 ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2018 http://yzncms.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: 御宅男 <530765310@qq.com>
-// +----------------------------------------------------------------------
 
-// +----------------------------------------------------------------------
-// | 邮箱模型
-// +----------------------------------------------------------------------
 namespace app\common\model;
 
 use think\Model;
 
+/**
+ * 邮箱验证码
+ */
 class Ems extends Model
 {
+
     protected $auto = ['ip'];
 
     public function setIpAttr($value)
     {
         return request()->ip();
-    }
-    /**
-     * 验证码有效时长
-     * @var int
-     */
-    protected $expire = 120;
-
-    /**
-     * 最大允许检测的次数
-     * @var int
-     */
-    protected $maxCheckNums = 10;
-
-    /**
-     * 获取最后一次邮箱发送的数据
-     *
-     * @param   int    $email 邮箱
-     * @param   string $event 事件
-     * @return  Ems
-     */
-    public function get($email, $event = 'default')
-    {
-        $ems = self::where(['email' => $email, 'event' => $event])
-            ->order('id', 'DESC')
-            ->find();
-        hook('ems_get', $ems);
-        return $ems ? $ems : null;
-    }
-
-    /**
-     * 发送验证码
-     *
-     * @param   int    $email 邮箱
-     * @param   int    $code  验证码,为空时将自动生成4位数字
-     * @param   string $event 事件
-     * @return  boolean
-     */
-    public function send($email, $code = null, $event = 'default')
-    {
-        $code   = is_null($code) ? mt_rand(1000, 9999) : $code;
-        $time   = time();
-        $ems    = self::create(['event' => $event, 'email' => $email, 'code' => $code, 'create_time' => $time]);
-        $result = hook('ems_send', $ems, true, true);
-        if (!$result) {
-            $ems->delete();
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * 校验验证码
-     *
-     * @param   int    $email 邮箱
-     * @param   int    $code  验证码
-     * @param   string $event 事件
-     * @return  boolean
-     */
-    public function check($email, $code, $event = 'default')
-    {
-        $time = time() - $this->expire;
-        $ems  = self::where(['email' => $email, 'event' => $event])
-            ->order('id', 'DESC')
-            ->find();
-        if ($ems) {
-            if ($ems['create_time'] > $time && $ems['times'] <= $this->maxCheckNums) {
-                $correct = $code == $ems['code'];
-                if (!$correct) {
-                    $ems->times = $ems->times + 1;
-                    $ems->save();
-                    return false;
-                } else {
-                    $result = hook('ems_check', $ems, true, true);
-                    return $result;
-                }
-            } else {
-                // 过期则清空该邮箱验证码
-                $this->flush($email, $event);
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * 清空指定邮箱验证码
-     *
-     * @param   int    $email 邮箱
-     * @param   string $event 事件
-     * @return  boolean
-     */
-    public function flush($email, $event = 'default')
-    {
-        self::where(['email' => $email, 'event' => $event])->delete();
-        hook('ems_flush');
-        return true;
     }
 
 }
