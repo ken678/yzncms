728599fc8249990ab914ea83f008dd606d7d8a72
ken678 <530765310@qq.com>
2021-10-11 16:14:37 +0800

    七牛云

--- addons/qiniu/config.php
+++ addons/qiniu/config.php
@@ -2,30 +2,30 @@
 return [
     0 => [
         'name'  => 'accessKey',
-        'title' => '七牛的ak：',
+        'title' => '七牛云的ak',
         'type'  => 'text',
         'value' => '',
-        'tip'   => '七牛的ak',
+        'tip'   => '七牛云的ak',
     ],
     1 => [
         'name'  => 'secrectKey',
-        'title' => '七牛的sk：',
+        'title' => '七牛云的sk',
         'type'  => 'text',
         'value' => '',
-        'tip'   => '七牛的sk',
+        'tip'   => '七牛云的sk',
     ],
     2 => [
         'name'  => 'bucket',
-        'title' => '七牛的空间名称：',
+        'title' => '七牛云的空间名称',
         'type'  => 'text',
         'value' => '',
-        'tip'   => '七牛的空间名称',
+        'tip'   => '七牛云的空间名称',
     ],
     3 => [
         'name'  => 'domain',
-        'title' => '七牛的空间对应的域名：',
+        'title' => '七牛云绑定的域名',
         'type'  => 'text',
         'value' => '',
-        'tip'   => '七牛的空间对应的域名，已http://开头',
+        'tip'   => '七牛云的空间对应的域名，已http://开头，/结尾',
     ],
 ];
