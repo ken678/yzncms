3e801626854671b65f0aed3c5da78919ce6993fb
ken678 <530765310@qq.com>
2021-10-11 15:53:40 +0800

    增加阿里云oss插件

---
+++ addons/alioss/config.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    0 => [
+        'name'  => 'accessKey',
+        'title' => '阿里云OSS的ak：',
+        'type'  => 'text',
+        'value' => '',
+        'tip'   => '阿里云OSS的ak',
+    ],
+    1 => [
+        'name'  => 'secrectKey',
+        'title' => '阿里云OSS的sk：',
+        'type'  => 'text',
+        'value' => '',
+        'tip'   => '阿里云OSS的sk',
+    ],
+    2 => [
+        'name'  => 'bucket',
+        'title' => '阿里云OSS空间Bucket名称：',
+        'type'  => 'text',
+        'value' => '',
+        'tip'   => '阿里云OSS空间Bucket名称',
+    ],
+    3 => [
+        'name'  => 'endpoint',
+        'title' => '阿里云OSS地域节点Endpoint：',
+        'type'  => 'text',
+        'value' => '',
+        'tip'   => '阿里云OSS地域节点Endpoint',
+    ],
+    4 => [
+        'name'  => 'domain',
+        'title' => '阿里云OSS空间对应绑定的域名：',
+        'type'  => 'text',
+        'value' => '',
+        'tip'   => '阿里云OSS空间对应绑定的域名，已http://开头，/结尾',
+    ],
+];
