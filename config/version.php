742e5b68ad96243a742c942b117ff2ffcbd79950
ken678 <530765310@qq.com>
2021-11-02 17:03:04 +0800

    版本日期

--- config/version.php
+++ config/version.php
@@ -15,6 +15,6 @@
 
 return [
     "yzncms_name"    => "YznCMS内容管理系统", //产品名称
-    "yzncms_release" => 20211019, //产品更新日期
+    "yzncms_release" => 20211102, //产品更新日期
     "yzncms_version" => "1.0.0", //产品版本号
 ];
