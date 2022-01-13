dbeba32ddf1b7bfab9f61c55621f325f090e6dbe
ken678 <530765310@qq.com>
2022-01-13 17:40:24 +0800

    安装文件

--- public/install.php
+++ public/install.php
@@ -20,16 +20,17 @@ if (version_compare(PHP_VERSION, '7.0.0', '<')) {
     die('PHP 7.0.0 及以上版本系统才可运行~ ');
 }
 
+define('IF_PUBLIC', true);
 define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
 define('APP_PATH', ROOT_PATH . 'application' . DIRECTORY_SEPARATOR);
 
 // 加载基础文件
 require ROOT_PATH . 'thinkphp' . DIRECTORY_SEPARATOR . 'base.php';
-
 // 执行应用并响应
 Container::get('app')->bind('\app\admin\command\Install')->run()->send();
 
 /*PS:如果你的网站空间不支持域名绑定目录
 1.请将index.php放置根目录(不推荐)
-2.第24行改成 define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
+2.注释第23行代码 define('IF_PUBLIC', true);
+3.第24行改成 define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
  */
