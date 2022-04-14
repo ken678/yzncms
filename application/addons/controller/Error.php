c1486616a599c7b632b474c6f97fa23f2a35a9d3
ken678 <530765310@qq.com>
2022-04-14 18:43:29 +0800

    Loader::parseName改成使用助手函数

--- application/addons/controller/Error.php
+++ application/addons/controller/Error.php
@@ -20,9 +20,9 @@ class Error
 {
     public function _empty(Request $request)
     {
-        $controller = \think\Loader::parseName($request->controller());
-        $action = $request->action();
-        $class = $request->param('isadmin/d') ? 'Admin' : 'Index';
+        $controller = parse_name($request->controller());
+        $action     = $request->action();
+        $class      = $request->param('isadmin/d') ? 'Admin' : 'Index';
         if (!plugin_action_exists($controller, $class, $action)) {
             throw new \think\Exception("插件{$controller}实例化错误！");
         }
