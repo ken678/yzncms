8835a3cea9114084e01500d2634c28f62a449a29
ken678 <530765310@qq.com>
2022-07-13 15:30:41 +0800

    字段验证取消-字符

--- application/cms/validate/ModelField.php
+++ application/cms/validate/ModelField.php
@@ -20,7 +20,7 @@ class ModelField extends Validate
 {
     //定义验证规则
     protected $rule = [
-        'name|字段名称'           => 'require|regex:/^[a-zA-Z][A-Za-z0-9\-\_]+$/',
+        'name|字段名称'           => 'require|regex:/^[a-zA-Z][A-Za-z0-9\_]+$/',
         'title|字段标题'          => 'require',
         'type|字段类型'           => 'require|alphaDash',
         'setting.define|字段定义' => 'require',
@@ -32,6 +32,6 @@ class ModelField extends Validate
 
     //定义验证提示
     protected $message = [
-        'name.regex' => '字段名称只能为字母、数字和下划线_及破折号-，并且仅能字母开头',
+        'name.regex' => '字段名称只能为字母、数字和下划线_，并且仅能字母开头',
     ];
 }
