87256052590de6309ecc018f978795360324d574
ken678 <530765310@qq.com>
2022-10-12 20:58:06 +0800

    插件更新

--- addons/member/application/member/model/Member.php
+++ addons/member/application/member/model/Member.php
@@ -68,49 +68,4 @@ class Member extends Model
         }
         return $value;
     }
-
-    /**
-     * 更新用户基本资料
-     * @param $username 用户名
-     * @param $oldpw 旧密码
-     * @param string $newpw 新密码，如不修改为空
-     * @param string $email 如不修改为空
-     * @param int $ignoreoldpw 是否忽略旧密码
-     * @param array $data 其他信息
-     * @return bool
-     */
-    public function userEdit($username, $oldpw, $newpw = '', $email = '', $ignoreoldpw = 0, $data = array())
-    {
-        //验证旧密码是否正确
-        if ($ignoreoldpw == 0) {
-            $info = self::where(["username" => $username])->find();
-            if (encrypt_password($oldpw, $info['encrypt']) != $info['password']) {
-                $this->error = '旧密码错误！';
-                return false;
-            }
-        }
-        if ($newpw) {
-            $passwordinfo = encrypt_password($newpw);
-            $data         = array(
-                "password" => $passwordinfo['password'],
-                "encrypt"  => $passwordinfo['encrypt'],
-            );
-        } else {
-            unset($data['password'], $data['encrypt']);
-        }
-        if ($email) {
-            $data['email'] = $email;
-        } else {
-            unset($data['email']);
-        }
-        if (empty($data)) {
-            return true;
-        }
-        if (self::allowField(true)->save($data, ["username" => $username]) !== false) {
-            return true;
-        } else {
-            $this->error = '用户资料更新失败！';
-            return false;
-        }
-    }
 }
