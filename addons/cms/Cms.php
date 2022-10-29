d0dbe005b74385946ced80844ac27393e766e44a
ken678 <530765310@qq.com>
2022-10-29 22:12:27 +0800

    插件更新

--- addons/cms/Cms.php
+++ addons/cms/Cms.php
@@ -360,19 +360,18 @@ class Cms extends Addons
     public function appInit()
     {
         $config = get_addon_config('cms');
-        Route::group('/', function () use ($config) {
-            Route::rule('', 'cms/index/index');
-            Route::rule('index', 'cms/index/index');
-            Route::rule('lists/:catid', 'cms/index/lists')->pattern(['catid' => '\d+']);
-            Route::rule('shows/:catid/:id', 'cms/index/shows')->pattern(['catid' => '\d+', 'id' => '\d+']);
-            Route::rule('tag/[:tag]', 'cms/index/tags');
-            Route::rule('search', 'cms/index/search');
-            if ($config['site_url_mode'] == 2) {
-                //Route::rule('admin', 'admin/index/login'); //如去除c/ d/ 需要解开此注释
-                Route::rule('d/:catdir/:id', 'cms/index/shows')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'id' => '\d+']);
-                Route::rule('c/:catdir/[:condition]', 'cms/index/lists')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'condition' => '[0-9_&=a-zA-Z]+']);
-            }
-        });
+
+        Route::rule('', 'cms/index/index');
+        Route::rule('index', 'cms/index/index');
+        Route::rule('lists/:catid', 'cms/index/lists')->pattern(['catid' => '\d+']);
+        Route::rule('shows/:catid/:id', 'cms/index/shows')->pattern(['catid' => '\d+', 'id' => '\d+']);
+        Route::rule('tag/[:tag]', 'cms/index/tags');
+        Route::rule('search', 'cms/index/search');
+        if ($config['site_url_mode'] == 2) {
+            //Route::rule('admin', 'admin/index/login'); //如去除c/ d/ 需要解开此注释
+            Route::rule('d/:catdir/:id', 'cms/index/shows')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'id' => '\d+']);
+            Route::rule('c/:catdir/[:condition]', 'cms/index/lists')->pattern(['catdir' => '[A-Za-z0-9\-\_]+', 'condition' => '[0-9_&=a-zA-Z]+']);
+        }
 
         //此函数需要全局调用
         if (is_file(ADDON_PATH . 'cms' . DS . 'function.php')) {
