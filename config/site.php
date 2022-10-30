4fab493dac90180f2a85f9891fb1c21b05602aa6
ken678 <530765310@qq.com>
2022-10-30 20:49:16 +0800

    插件更新

---
+++ config/site.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'web_site_icp'                     => '',
+    'web_site_statistics'              => '',
+    'config_group'                     => [
+        'base'    => '基础',
+        'system'  => '系统',
+        'upload'  => '上传',
+        'develop' => '开发',
+    ],
+    'theme'                            => 'default',
+    'admin_forbid_ip'                  => '',
+    'upload_image_size'                => '0',
+    'upload_image_ext'                 => 'gif,jpg,jpeg,bmp,png',
+    'upload_file_size'                 => '0',
+    'upload_file_ext'                  => 'doc,docx,xls,xlsx,ppt,pptx,pdf,wps,txt,rar,zip,gz,bz2,7z',
+    'upload_driver'                    => 'local',
+    'upload_driver_text'               => '本地',
+    'upload_thumb_water'               => '0',
+    'upload_thumb_water_pic'           => '',
+    'upload_thumb_water_position'      => '9',
+    'upload_thumb_water_position_text' => '右下角',
+    'upload_thumb_water_alpha'         => '50',
+];
