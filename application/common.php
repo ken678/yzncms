a1b608659c2ff41b3b881e082d0ae2115b497585
ken678 <530765310@qq.com>
2022-11-23 14:15:55 +0800

    内置首字母头像

--- application/common.php
+++ application/common.php
@@ -690,6 +690,25 @@ function hsv2rgb($h, $s, $v)
 }
 
 /**
+ * 首字母头像
+ * @param $text
+ * @return string
+ */
+function letter_avatar($text)
+{
+    $total           = unpack('L', hash('adler32', $text, true))[1];
+    $hue             = $total % 360;
+    list($r, $g, $b) = hsv2rgb($hue / 360, 0.3, 0.9);
+
+    $bg    = "rgb({$r},{$g},{$b})";
+    $color = "#ffffff";
+    $first = mb_strtoupper(mb_substr($text, 0, 1));
+    $src   = base64_encode('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="100" width="100"><rect fill="' . $bg . '" x="0" y="0" width="100" height="100"></rect><text x="50" y="50" font-size="50" text-copy="fast" fill="' . $color . '" text-anchor="middle" text-rights="admin" alignment-baseline="central">' . $first . '</text></svg>');
+    $value = 'data:image/svg+xml;base64,' . $src;
+    return $value;
+}
+
+/**
  * 生成文件后缀图片
  * @param string $suffix 后缀
  * @param null   $background
