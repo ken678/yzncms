cf8b1204e229d039a0bf39f844619bd86e6b48fe
ken678 <530765310@qq.com>
2022-11-23 20:47:53 +0800

    接口验证

--- application/common/controller/Api.php
+++ application/common/controller/Api.php
@@ -16,7 +16,9 @@ namespace app\common\controller;
 
 use think\App;
 use think\exception\HttpResponseException;
+use think\exception\ValidateException;
 use think\Response;
+use think\Validate;
 
 class Api
 {
@@ -112,6 +114,56 @@ class Api
         }
         $response = Response::create($result, $type, $code)->header($header);
         throw new HttpResponseException($response);
+    }
+
+    /**
+     * 验证数据
+     * @access protected
+     * @param  array        $data     数据
+     * @param  string|array $validate 验证器名或者验证规则数组
+     * @param  array        $message  提示信息
+     * @param  bool         $batch    是否批量验证
+     * @param  mixed        $callback 回调方法（闭包）
+     * @return array|string|true
+     * @throws ValidateException
+     */
+    protected function validate($data, $validate, $message = [], $batch = false, $callback = null)
+    {
+        if (is_array($validate)) {
+            $v = new Validate();
+            $v->rule($validate);
+        } else {
+            if (strpos($validate, '.')) {
+                // 支持场景
+                list($validate, $scene) = explode('.', $validate);
+            }
+            $v = new Validate($validate);
+            if (!empty($scene)) {
+                $v->scene($scene);
+            }
+        }
+
+        // 是否批量验证
+        if ($batch || $this->batchValidate) {
+            $v->batch(true);
+        }
+
+        if (is_array($message)) {
+            $v->message($message);
+        }
+
+        if ($callback && is_callable($callback)) {
+            call_user_func_array($callback, [$v, &$data]);
+        }
+
+        if (!$v->check($data)) {
+            if ($this->failException) {
+                throw new ValidateException($v->getError());
+            }
+            return $v->getError();
+        }
+
+        return true;
     }
 
     //刷新Token
