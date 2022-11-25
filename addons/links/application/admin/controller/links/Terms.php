2d69996f01c9d99d7ff7f04b0cc5bf0e5d71b658
ken678 <530765310@qq.com>
2022-11-25 14:11:30 +0800

    优化友情链接插件

---
+++ addons/links/application/admin/controller/links/Terms.php
@@ -0,0 +1,57 @@
+<?php
+// +----------------------------------------------------------------------
+// | Yzncms [ 御宅男工作室 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://yzncms.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 御宅男 <530765310@qq.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | 友情链接管理
+// +----------------------------------------------------------------------
+namespace app\admin\controller\links;
+
+use app\admin\model\links\Terms as TermsModel;
+use app\common\controller\Adminbase;
+
+/**
+ * 友情链接管理
+ */
+class Terms extends Adminbase
+{
+    protected function initialize()
+    {
+        parent::initialize();
+        $this->modelClass = new TermsModel;
+    }
+
+    //分类管理
+    public function index()
+    {
+        if ($this->request->isAjax()) {
+            list($page, $limit, $where) = $this->buildTableParames();
+            $order                      = $this->request->param("order/s", "DESC");
+            $sort                       = $this->request->param("sort", 'id');
+
+            $count = $this->modelClass
+                ->where($where)
+                ->where("module", "links")
+                ->order($sort, $order)
+                ->count();
+
+            $data = $this->modelClass
+                ->where($where)
+                ->where("module", "links")
+                ->order($sort, $order)
+                ->page($page, $limit)
+                ->select();
+
+            $result = ["code" => 0, 'count' => $count, 'data' => $data];
+            return json($result);
+        }
+        return $this->fetch();
+    }
+}
