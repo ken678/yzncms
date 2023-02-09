3cad36a58f42478ca115f69a301ac0b633c021a8
ken678 <530765310@qq.com>
2023-02-09 14:24:31 +0800

    地区插件增加高德api更新

---
+++ addons/area/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    [
+        'name'  => 'amap_webapi_key',
+        'title' => '高德地图的KEY',
+        'type'  => 'text',
+        'value' => '',
+        'tip'   => '',
+    ],
+];
