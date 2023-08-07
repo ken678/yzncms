100b56cc0b933f86f9efd0a3d6a737cbf2b2bdfb
ken678 <530765310@qq.com>
2023-08-07 10:22:27 +0800

    ..

--- application/admin/validate/AuthRule.php
+++ application/admin/validate/AuthRule.php
@@ -10,29 +10,18 @@
 // +----------------------------------------------------------------------
 namespace app\admin\validate;
 
-use \think\Validate;
+use think\Validate;
 
 /**
- * 用户验证器
+ * 菜单验证器
  */
-class Menu extends Validate
+class AuthRule extends Validate
 {
     //定义验证规则
     protected $rule = [
-        'parentid|上级菜单'  => 'require|number',
-        'title|名称'       => 'require|chsAlphaNum',
-        'app|模块'         => 'require|alpha',
-        'controller|控制器' => 'require',
-        'action|方法'      => 'require|alphaNum',
+        'pid|上级菜单'  => 'require|number',
+        'name|规则'       => 'require',
+        'title|标题'       => 'require',
     ];
 
-    //定义验证提示
-    protected $message = [
-    ];
-
-    //定义验证场景
-    protected $scene = [
-        'add'  => ['parentid', 'title', 'app', 'controller', 'action'],
-        'edit' => ['parentid', 'title', 'app', 'controller', 'action'],
-    ];
 }
