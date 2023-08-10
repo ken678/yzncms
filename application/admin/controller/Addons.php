9d6eb0259b907a2de643794937840d046e3bb5d0
ken678 <530765310@qq.com>
2023-08-10 10:16:08 +0800

    ..

--- application/admin/controller/Addons.php
+++ application/admin/controller/Addons.php
@@ -12,7 +12,7 @@
 // +----------------------------------------------------------------------
 // | 插件管理
 // +----------------------------------------------------------------------
-namespace app\admin\controller\addons;
+namespace app\admin\controller;
 
 use app\common\controller\Adminbase;
 use think\addons\AddonException;
