70f17bf5f5cdf5e153ea9e06fea4738dfa880b87
ken678 <530765310@qq.com>
2024-02-22 16:13:09 +0800

    后台日志

--- application/admin/controller/auth/Adminlog.php
+++ application/admin/controller/auth/Adminlog.php
@@ -19,10 +19,14 @@ use app\common\controller\Adminbase;
 
 class Adminlog extends Adminbase
 {
+    protected $modelClass       = null;
+    protected $childrenAdminIds = [];
+
     protected function initialize()
     {
         parent::initialize();
-        $this->modelClass = new AdminlogModel;
+        $this->modelClass       = new AdminlogModel;
+        $this->childrenAdminIds = $this->auth->getChildrenAdminIds(true);
     }
 
     //删除一个月前的操作日志
@@ -30,6 +34,24 @@ class Adminlog extends Adminbase
     {
         AdminlogModel::where('create_time', '<= time', time() - (86400 * 30))->delete();
         $this->success("删除日志成功！");
+    }
+
+    /**
+     * 详情
+     */
+    public function detail($id)
+    {
+        $row = $this->modelClass->get($id);
+        if (!$row) {
+            $this->error('记录未找到');
+        }
+        if (!$this->auth->isAdministrator()) {
+            if (!$row['admin_id'] || !in_array($row['admin_id'], $this->childrenAdminIds)) {
+                $this->error('你没有权限访问');
+            }
+        }
+        $this->assign("row", $row->toArray());
+        return $this->fetch();
     }
 
     //添加
