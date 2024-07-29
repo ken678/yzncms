1c7061cdd23b9f5921cecfb7ef7a2c238e54ca05
ken678 <530765310@qq.com>
2024-07-29 15:19:10 +0800

    ..

--- config/cache.php
+++ config/cache.php
@@ -10,7 +10,8 @@ return [
 
     // 缓存连接方式配置
     'stores'  => [
-        'file' => [
+        // 文件缓存
+        'file'  => [
             // 驱动方式
             'type'       => 'File',
             // 缓存保存目录
@@ -24,6 +25,27 @@ return [
             // 序列化机制 例如 ['serialize', 'unserialize']
             'serialize'  => [],
         ],
-        // 更多的缓存连接
+        // redis缓存(推荐)
+        'redis' => [
+            // 驱动方式
+            'type'       => 'redis',
+            // 服务器地址
+            'host'       => '127.0.0.1',
+            // 服务器的端口号，默认为 6379
+            'port'       => 6379,
+            // 如果未设置密码，则为空字符串
+            'password'   => '',
+            // 数据库编号，默认为 0（第一个数据库）
+            'select'     => 0,
+            // 缓存前缀
+            'prefix'     => '',
+            // 缓存有效期 0表示永久缓存
+            'expire'     => 0,
+            // 缓存标签前缀
+            'tag_prefix' => 'tag:',
+            // 序列化机制 例如 ['serialize', 'unserialize']
+            'serialize'  => [],
+        ],
+        // 更多的缓存连接 memcache、wincache、sqlite
     ],
 ];
