a35c4ab5ba8b5728d8db9b4a0bd6f5424b27e157
ken678 <530765310@qq.com>
2024-09-27 12:17:22 +0800

    ..

--- app/admin/model/AuthGroup.php
+++ app/admin/model/AuthGroup.php
@@ -14,47 +14,9 @@
 // +----------------------------------------------------------------------
 namespace app\admin\model;
 
-use think\Exception;
 use think\Model;
 
 class AuthGroup extends Model
 {
-    protected static $roleList = [];
 
-    public static function onBeforeDelete($row)
-    {
-        if ($row->id == 1) {
-            throw new Exception("超级管理员角色不能被删除！");
-        }
-        $admin = AdminUser::where('roleid', $row->id)->find();
-        if ($admin) {
-            throw new Exception("该角色下有管理员！");
-        }
-        //子角色列表
-        $child = explode(',', self::getArrchildid($row->id));
-        if (count($child) > 1) {
-            throw new Exception("该角色下有子角色，请删除子角色才可以删除！");
-        }
-    }
-
-    /**
-     * 通过递归的方式获取该角色下的全部子角色
-     * @param type $id
-     * @return string
-     */
-    public static function getArrchildid($id)
-    {
-        if (empty(self::$roleList)) {
-            self::$roleList = self::order(["id" => "desc"])->column('*', 'id');
-        }
-        $arrchildid = $id;
-        if (is_array(self::$roleList)) {
-            foreach (self::$roleList as $k => $cat) {
-                if ($cat['parentid'] && $k != $id && $cat['parentid'] == $id) {
-                    $arrchildid .= ',' . self::getArrchildid($k);
-                }
-            }
-        }
-        return $arrchildid;
-    }
 }
