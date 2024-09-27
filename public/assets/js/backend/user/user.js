ee69e2c77b67d216ee300fc7fea102b17b64bfce
ken678 <530765310@qq.com>
2024-09-27 14:11:43 +0800

    ..

--- public/assets/js/backend/user/user.js
+++ public/assets/js/backend/user/user.js
@@ -25,10 +25,9 @@ define(['jquery', 'table', 'form'], function($, Table, Form) {
                         { field: 'mobile', width: 120, title: '手机', searchOp: 'like' },
                         { field: 'email',  width: 180,title: '邮箱', searchOp: 'like' },
                         { field: 'reg_ip',  width: 120,title: '注册IP' },
-                        { field: 'last_login_time', width: 170, title: '最后登录', search: 'range', templet: Table.formatter.datetime },
+                        { field: 'last_login_time', width: 160, title: '最后登录', search: 'range', templet: Table.formatter.datetime },
                         { field: 'amount', width: 90, title: '金钱总数' },
                         { field: 'point', width: 90, title: '积分总数' },
-                        { field: 'login', width: 90, title: '登录次数' },
                         { field: 'status', width: 80, title: '状态', templet: Table.formatter.status, selectList: { 0: '禁用', 1: '正常' }, search: false },
                         { fixed: 'right', width: 90, title: '操作', templet: Table.formatter.tool, operat: ['edit', 'delete'] }
                     ]
