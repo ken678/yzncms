7647c760ad1dc39fb2122816e0f226d2f4311bcf
ken678 <530765310@qq.com>
2024-09-27 11:44:25 +0800

    ..

--- app/admin/controller/auth/Manager.php
+++ app/admin/controller/auth/Manager.php
@@ -16,10 +16,12 @@
 // +----------------------------------------------------------------------
 namespace app\admin\controller\auth;
 
-use app\admin\model\AdminUser as Admin_User;
-use app\admin\model\AuthGroup as AuthGroupModel;
+use app\admin\model\AdminUser;
+use app\admin\model\AuthGroup;
+use app\admin\model\AuthGroupAccess;
 use app\common\controller\Backend;
 use think\exception\ValidateException;
+use think\facade\Db;
 use util\Tree;
 
 class Manager extends Backend
@@ -27,33 +29,17 @@ class Manager extends Backend
     protected $searchFields     = 'id,username,nickname';
     protected $childrenGroupIds = [];
     protected $childrenAdminIds = [];
+    //无需要权限判断的方法
+    protected $noNeedRight = ['get_group_list'];
+
     protected function initialize()
     {
         parent::initialize();
-        $this->modelClass = new Admin_User;
+        $this->modelClass = new AdminUser;
 
         $this->childrenAdminIds = $this->auth->getChildrenAdminIds(true);
         $this->childrenGroupIds = $this->auth->getChildrenGroupIds(true);
 
-        $groupList = AuthGroupModel::where('id', 'in', $this->childrenGroupIds)->select()->toArray();
-        Tree::instance()->init($groupList);
-        $groupdata = [];
-        if ($this->auth->isAdministrator()) {
-            $result = Tree::instance()->getTreeList(Tree::instance()->getTreeArray(0), 'title');
-            foreach ($result as $k => $v) {
-                $groupdata[$v['id']] = $v['title'];
-            }
-        } else {
-            $result = [];
-            $groups = $this->auth->getGroups();
-            foreach ($groups as $m => $n) {
-                $childlist = Tree::instance()->getTreeList(Tree::instance()->getTreeArray($n['id']), 'title');
-                foreach ($childlist as $k => $v) {
-                    $groupdata[$v['id']] = $v['title'];
-                }
-            }
-        }
-        $this->assign('groupdata', $groupdata);
         $this->assignconfig("admin", ['id' => $this->auth->id]);
     }
 
@@ -67,11 +53,24 @@ class Manager extends Backend
             if ($this->request->request('keyField')) {
                 return $this->selectpage();
             }
-            [$page, $limit, $where, $sort, $order] = $this->buildTableParames();
 
             $childrenGroupIds = $this->childrenGroupIds;
-            $groupName        = AuthGroupModel::where('id', 'in', $childrenGroupIds)
+            $groupName        = AuthGroup::where('id', 'in', $childrenGroupIds)
                 ->column('title', 'id');
+            $authGroupList = AuthGroupAccess::where('group_id', 'in', $childrenGroupIds)
+                ->field('uid,group_id')
+                ->select();
+            $adminGroupName = [];
+            foreach ($authGroupList as $k => $v) {
+                if (isset($groupName[$v['group_id']])) {
+                    $adminGroupName[$v['uid']][$v['group_id']] = $groupName[$v['group_id']];
+                }
+            }
+            $groups = $this->auth->getGroups();
+            foreach ($groups as $m => $n) {
+                $adminGroupName[$this->auth->id][$n['id']] = $n['title'];
+            }
+            [$page, $limit, $where, $sort, $order] = $this->buildTableParames();
 
             $list = $this->modelClass
                 ->where($where)
@@ -81,7 +80,9 @@ class Manager extends Backend
                 ->paginate($limit);
 
             foreach ($list as $k => &$v) {
-                $v['groups'] = $groupName[$v['roleid']] ?? '未知';
+                $groups           = isset($adminGroupName[$v['id']]) ? $adminGroupName[$v['id']] : [];
+                $v['groups']      = implode(',', array_keys($groups));
+                $v['groups_text'] = implode(',', array_values($groups));
             }
             unset($v);
             $result = ["code" => 0, 'count' => $list->total(), "data" => $list->items()];
@@ -98,21 +99,21 @@ class Manager extends Backend
         if ($this->request->isPost()) {
             $this->token();
             $params = $this->request->param('row/a');
+            Db::startTrans();
             try {
                 $this->validate($params, 'AdminUser.insert');
-            } catch (ValidateException $e) {
-                $this->error($e->getMessage());
-            }
-            $passwordinfo       = encrypt_password($params['password']); //对密码进行处理
-            $params['password'] = $passwordinfo['password'];
-            $params['encrypt']  = $passwordinfo['encrypt'];
 
-            if (!in_array($params['roleid'], $this->childrenGroupIds)) {
-                $this->error('没有权限操作！');
-            }
-            try {
+                $passwordinfo       = encrypt_password($params['password']); //对密码进行处理
+                $params['password'] = $passwordinfo['password'];
+                $params['encrypt']  = $passwordinfo['encrypt'];
+
+                /*if (!in_array($params['roleid'], $this->childrenGroupIds)) {
+                throw new \Exception('没有权限操作！');
+                }*/
                 $this->modelClass->save($params);
-            } catch (\Exception $e) {
+                Db::commit();
+            } catch (ValidateException | \Exception $e) {
+                Db::rollback();
                 $this->error($e->getMessage());
             }
             $this->success("添加成功！", url('index'));
@@ -136,31 +137,33 @@ class Manager extends Backend
         if ($this->request->isPost()) {
             $this->token();
             $params = $this->request->param('row/a');
+            Db::startTrans();
             try {
                 $this->validate($params, 'AdminUser.update');
-            } catch (ValidateException $e) {
-                $this->error($e->getMessage());
-            }
-            if (!in_array($params['roleid'], $this->childrenGroupIds)) {
+                /*if (!in_array($params['roleid'], $this->childrenGroupIds)) {
                 $this->error('没有权限操作！');
-            }
-            //密码为空，表示不修改密码
-            if (isset($params['password']) && $params['password']) {
-                $passwordinfo       = encrypt_password($params['password']); //对密码进行处理
-                $params['encrypt']  = $passwordinfo['encrypt'];
-                $params['password'] = $passwordinfo['password'];
+                }*/
+                //密码为空，表示不修改密码
+                if (isset($params['password']) && $params['password']) {
+                    $passwordinfo       = encrypt_password($params['password']); //对密码进行处理
+                    $params['encrypt']  = $passwordinfo['encrypt'];
+                    $params['password'] = $passwordinfo['password'];
 
-            } else {
-                unset($params['password'], $params['encrypt']);
-            }
-            try {
+                } else {
+                    unset($params['password'], $params['encrypt']);
+                }
                 $row->save($params);
-            } catch (\Exception $e) {
+                Db::commit();
+            } catch (ValidateException | \Exception $e) {
+                Db::rollback();
                 $this->error($e->getMessage());
             }
             $this->success("修改成功！");
         }
+
+        $groupIds = $this->auth->getGroupIds($row['id']);
         $this->assign("data", $row);
+        $this->view->assign("groupids", implode(',', $groupIds));
         return $this->fetch();
     }
 
@@ -202,6 +205,38 @@ class Manager extends Backend
     {
         // 管理员禁止批量操作
         $this->error();
+    }
+
+    public function get_group_list()
+    {
+        $keyValue = $this->request->param('keyValue');
+        $where    = [];
+        if ($keyValue) {
+            $groupList = AuthGroup::where('id', 'in', $keyValue)->select()->toArray();
+            foreach ($groupList as $k => $v) {
+                $groupdata[] = ['id' => $v['id'], 'name' => $v['title']];
+            }
+            return $result = ['count' => count($groupdata), 'data' => $groupdata];
+        }
+        $groupList = AuthGroup::where('id', 'in', $this->childrenGroupIds)->select()->toArray();
+        Tree::instance()->init($groupList);
+        $groupdata = [];
+        if ($this->auth->isAdministrator()) {
+            $result = Tree::instance()->getTreeList(Tree::instance()->getTreeArray(0), 'title');
+            foreach ($result as $k => $v) {
+                $groupdata[] = ['id' => $v['id'], 'name' => $v['title']];
+            }
+        } else {
+            $result = [];
+            $groups = $this->auth->getGroups();
+            foreach ($groups as $m => $n) {
+                $childlist = Tree::instance()->getTreeList(Tree::instance()->getTreeArray($n['id']), 'title');
+                foreach ($childlist as $k => $v) {
+                    $groupdata[] = ['id' => $v['id'], 'name' => $v['title']];
+                }
+            }
+        }
+        return $result = ['count' => count($groupdata), 'data' => $groupdata];
     }
 
 }
