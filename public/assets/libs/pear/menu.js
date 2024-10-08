3fde0d74f7041556f5ace7bd927c52391e65e012
ken678 <530765310@qq.com>
2024-10-08 11:23:53 +0800

    ..

--- public/assets/libs/pear/menu.js
+++ public/assets/libs/pear/menu.js
@@ -387,7 +387,7 @@ define(['jquery', 'layui'], function($, layui) {
 						'</span></a>';
 				} else if (note.type == 1) {
 					// 创 建 菜 单 结 构
-					content += '<a ' + target + ' class="' + className + '" menu-type="' + note.type + '" menu-url="' + note.href +
+					content += '<a ' + note.extend + target + ' class="' + className + '" menu-type="' + note.type + '" menu-url="' + note.href +
 						'" menu-id="' + note.id +
 						'" menu-title="' + note.title + '" href="' + href + '"><i class="' + note.icon +
 						'"></i><span>' + note.title + '</span><div class="pull-right-container">' + note.badge + '</div></a>';
