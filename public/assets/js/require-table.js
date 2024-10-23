bffe77e61faac7bdbf89430d0cf3a636314b21f4
ken678 <530765310@qq.com>
2024-10-23 15:55:38 +0800

    ..

--- public/assets/js/require-table.js
+++ public/assets/js/require-table.js
@@ -467,6 +467,14 @@ define(['jquery', 'layui'], function($, layui) {
                 });
                 return false;
             })
+
+            // 重置事件
+            $('[data-table-reset="' + tableId + '"]').on('click', function () {
+                var options = layui.table.getOptions(tableId);
+                $('[data-field] li', $('div[lay-filter="' + options.layFilter + '"]')).removeClass('layui-this');
+                $('[data-field] li:first', $('div[lay-filter="' + options.layFilter + '"]')).addClass('layui-this');
+            })
+
             //表格点击搜索
             $(document).on("click", ".searchit", function() {
                 var value = $(this).data("value");
